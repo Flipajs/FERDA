2223bc3788226a8c790301bad19d5ef997bb1065
flipajs <filip.naiser@gmail.com>
2014-11-11 10:27:03 +0100

    Normalize all the line endings

---
+++ scripts/bg_model.py
@@ -0,0 +1,32 @@
+from utils import video_manager
+
+__author__ = 'flipajs'
+
+import numpy as np
+import cv2
+
+vid = video_manager.VideoManager("/home/flipajs/Dropbox/PycharmProjects/data/eight/eight.m4v")
+#vid = video_manager.VideoManager("/home/flipajs/Dropbox/PycharmProjects/data/NoPlasterNoLid800/NoPlasterNoLid800.m4v")
+
+bg = None
+i = -1
+while True:
+    i += 1
+    img = vid.move2_next()
+
+    if img is None:
+        break
+
+    if i % 50 != 0:
+        continue
+
+    if bg != None:
+        bg = np.maximum(bg, img)
+    else:
+        bg = img
+
+
+
+cv2.imshow("BG", bg)
+cv2.waitKey(0)
+cv2.imwrite('bg.png', bg)
