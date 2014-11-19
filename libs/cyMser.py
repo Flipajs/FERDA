9b0bd973f0306285ecd7d03a7397411297afbe9c
flipajs <filip.naiser@gmail.com>
2014-11-19 16:17:55 +0100

    Barbara vs Filip config added

--- libs/cyMser.py
+++ libs/cyMser.py
@@ -1,10 +1,15 @@
 __author__ = 'filip@naiser.cz'
 
+from utils.misc import is_flipajs_pc
+
 def __bootstrap__():
    global __bootstrap__, __loader__, __file__
    import sys, pkg_resources, imp
-   # __file__ = pkg_resources.resource_filename(__name__, 'cyMser_debian.so')
-   __file__ = pkg_resources.resource_filename(__name__, 'cyMser_trace_merge_tree.so')
+   if is_flipajs_pc():
+        __file__ = pkg_resources.resource_filename(__name__, 'cyMser_trace_merge_tree.so')
+   else:
+        __file__ = pkg_resources.resource_filename(__name__, 'cyMser_debian.so')
+
    __loader__ = None; del __bootstrap__, __loader__
    imp.load_dynamic(__name__,__file__)
 __bootstrap__()
