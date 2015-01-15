710d67eedd308fe763b11e36d282a24a0b2bc820
flipajs <filip.naiser@gmail.com>
2015-01-15 14:00:20 +0100

    print frame number test

--- utils/video_manager.py
+++ utils/video_manager.py
@@ -90,6 +90,7 @@ class VideoManager():
             return ret
 
     def seek_frame(self, frame_number):
+        print frame_number
         if frame_number < 0 or frame_number >= self.total_frame_count():
             raise Exception("Frame_number is invalid")
 
