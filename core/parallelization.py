ab1377b4ad0d96ba373fd0b085c7112b62c1b3d3
Flipajs <filip@naiser.cz>
2015-05-25 12:02:51 +0200

    development checkpoint 16

--- core/parallelization.py
+++ core/parallelization.py
@@ -5,7 +5,7 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
 import sys
-# from sklearn import svm
+from sklearn import svm
 from utils.video_manager import get_auto_video_manager
 import cPickle as pickle
 from core.region.mser import get_msers_
