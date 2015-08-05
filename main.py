8142a4a2289ca14812c4c7a0fe25c188c4b3bac0
Filip Naiser <filip@naiser.cz>
2015-08-05 15:24:10 +0200

    ..

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ S_.general.print_log = False
 
 # This is development speed up process (kind of fast start). Runs only on developers machines...
 if is_flipajs_pc():
-    project.load('/Users/flipajs/Documents/wd/colonies1_30m/colonies30m.fproj')
+
+    project.load('/Users/flipajs/Documents/wd/eight_test/test.fproj')
     ex.widget_control('load_project', project)
     pass
 
