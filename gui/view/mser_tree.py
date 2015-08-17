9e1b6404074aed83ba6892e19b30ec897e702927
Filip Naiser <filip@naiser.cz>
2015-08-17 14:05:18 +0200

    prepared..

---
+++ gui/view/mser_tree.py
@@ -0,0 +1,52 @@
+__author__ = 'flipajs'
+
+import sys
+import cv2
+from core.region.mser import get_msers_
+from core.project.project import Project
+from PyQt4 import QtGui, QtCore
+
+
+class MSERTree(QtGui.QWidget):
+    def __init__(self, img):
+        super(MSERTree, self).__init__()
+
+        self.setLayout(QtGui.QVBoxLayout())
+
+        self.view = QtGui.QGraphicsView()
+        self.scene = QtGui.QGraphicsScene()
+
+        self.view.setScene(self.scene)
+        self.layout().addWidget(self.view)
+
+        # TODO: z gui/view/graph_visualizer.py kolem line 60... vykresleni oblasti
+        # z scripts/region_graph3.py kolem line 307 fce show_node... tam se da zjistit jak
+        # pridat obrazek do sceny na nejakou pozici a jak ho mit klikaci...
+
+        pass
+
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+
+    # change for some test image...
+    im = cv2.imread('/Users/flipajs/Desktop/red_vid.png')
+    p = Project()
+    p.mser_parameters.min_area = 30
+    p.mser_parameters.min_margin = 5
+
+    regions = get_msers_(im, p)
+
+    for r in regions:
+        print r.area(), r.label()
+
+    ex = MSERTree(None)
+    ex.show()
+    ex.move(-500, -500)
+    ex.showMaximized()
+    ex.setFocus()
+
+    app.exec_()
+    app.deleteLater()
+    sys.exit()
