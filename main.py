1a5d32ff15aaf9fa0459991fdf608c33faa5ca67
Filip Naiser <filip@naiser.cz>
2015-08-21 11:19:35 +0200

    ..

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ if is_flipajs_pc():
     # project.load('/Users/flipajs/Documents/wd/F1C51/f1c51.fproj')
     # project.load('/Users/flipajs/Documents/wd/video_bounds_test/test.fproj')
     # project.load('/Users/flipajs/Documents/wd/small_lenses_colony1/small_lenses    .fproj')
-    project.load('/Users/flipajs/Documents/wd/eight/eight.fproj')
+    # project.load('/Users/flipajs/Documents/wd/eight/eight.fproj')
     # # # # # #
 
-    ex.widget_control('load_project', project)
+    # ex.widget_control('load_project', project)
 
     pass
 
