1a3a7297f894b913cff989e7fb884f253fc330c3
Dita Hollmannova <holl.dita@gmail.com>
2015-09-02 14:23:49 +0200

    fixed search, all conflicts are now found

--- gui/settings/shortcuts.py
+++ gui/settings/shortcuts.py
@@ -8,17 +8,21 @@ class Node():
         self.id = id
         # choose some random "shortcuts" (represented by ints)
         self.shortcuts = []
-        for i in range(0, random.randint(2, 5)):
-            self.shortcuts.append(random.randint(0, 20))
+        for i in range(0, random.randint(1, 3)):
+            rnd = random.randint(0, 20)
+            while rnd in self.shortcuts:
+                rnd = random.randint(0, 20)
+            self.shortcuts.append(rnd)
+        self.shortcuts.sort()
         self.children = []
         self.children_limit = random.randint(0, 3)
-        self.status = "fresh"
 
     def add_child(self, node):
         self.children.append(node)
 
     def children_count(self):
         return len(self.children)
+
 
 def pop(stack):
     return stack.pop()
@@ -31,6 +35,7 @@ def peek(stack):
 
 
 def create_graph():
+    random.seed(1)
     root = Node(0, "root")
     n = copy.copy(root)
     stack = []
@@ -52,13 +57,12 @@ def create_graph():
                 n.add_child(new)
                 stack.append(new)
                 # print "Adding a child to %s" % n.id
+                print "Adding a child to %s. ID is %s and shortcuts are %s. It will have %s children" % (n.id, new.id, new.shortcuts, new.children_limit)
                 n = new
                 depth += 1
                 id += 1
             else:
                 # close this node
-                # print "%s already has %s children" % (n.id, n.children_count())
-
                 try:
                     pop(stack)
                     n = peek(stack)
@@ -70,22 +74,31 @@ def create_graph():
     return root
 
 
-def search_graph(first_node):
-    first_node.status = "open"
-    for child in first_node.children:
+def search_graph(parent):
+    # temporary storage for all children's shortcuts
+    short = []
+    for child in parent.children:
+
         if len(child.children) > 0:
-            ok = True
+            # load child's children
             shortcuts = search_graph(child)
+            # check all shortcuts
             for sh in shortcuts:
+                # check if it collides with any key in child.shortcuts
                 if sh in child.shortcuts:
-                    ok = False
                     print "Conflict! Key %s from %s" % (sh, child.id)
-                else:
-                    child.shortcuts.append(sh)
-            return child.shortcuts
-        else:
-            print "end found!"
-            return child.shortcuts
+                # add it to short
+                if sh not in short:
+                    short.append(sh)
+
+        for sh in child.shortcuts:
+            if sh not in short:
+                short.append(sh)
+
+    # return all keys used in parent's children
+    short.sort()
+    # print "Children of %s have the following shortcuts: %s" % (parent.id, short)
+    return short
 
 
 t = time.time()
