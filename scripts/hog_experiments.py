42f8f9f3e7e84bc2a6886996255bcaad2dfc421b
Filip Naiser <filip@naiser.cz>
2015-09-10 13:19:28 +0200

    hog_experiments progress

--- scripts/hog_experiments.py
+++ scripts/hog_experiments.py
@@ -17,7 +17,7 @@ import cv2
 from PyQt4 import QtGui, QtCore
 import sys
 import cPickle as pickle
-from utils.roi import ROI
+from utils.roi import get_roi
 
 
 def get_mser(im, p):
@@ -61,14 +61,21 @@ def get_regions(project, solver, from_t, to_t):
     return reconstructed
 
 def warp_region(r, im, dst_h=16, dst_w=48):
-    roi = ROI(r)
+    roi = get_roi(r.pts())
     tl = roi.top_left_corner()-np.array([1, 1])
     br = roi.bottom_right_corner()+np.array([1, 1])
-    crop = im[tl[0]:br[0], tl[1]:br[1], :].copy()
+
+    # im_ = np.zeros_like(im)
+    # im_[r.pts()[:, 0], r.pts()[:, 1]] = im_[r.pts()[:, 0], r.pts()[:, 1]]
+
+    crop = im[tl[0]:br[0], tl[1]:br[1]].copy()
 
     p_ = np.array([r.a_*math.sin(-r.theta_), r.a_*math.cos(-r.theta_)])
     head = np.ceil(r.centroid() + p_) + np.array([1, 1])
     back = np.ceil(r.centroid() - p_) - np.array([1, 1])
+
+    if head[0] < back[0]:
+        head, back = back, head
     
     b_ = r.b_*2.5
     p_ = np.array([b_*math.sin(-r.theta_+np.pi+np.pi/2), b_*math.cos(-r.theta_+np.pi+np.pi/2)])
@@ -77,15 +84,33 @@ def warp_region(r, im, dst_h=16, dst_w=48):
     bl_c = back - p_
     br_c = head - p_
 
+    # src_pts = np.float32(np.array([tl_c, tr_c, br_c, bl_c]))
+    # # srcP = np.float32(np.array([[0, 0], [dst_h, 0], [dst_h, dst_w], [0, dst_w]]))
+    # dst_pts = np.float32(np.array([[0, 0], [0, dst_w], [dst_h, dst_w], [dst_h, 0]]))
+    # # dstP = np.float32(np.array([[-30, 50], [m-50, -50], [m+80, n-100], [150, n+50]]))
+    #
+    # src_pts[:, 0], src_pts[:, 1] = src_pts[:, 1].copy(), src_pts[:, 0].copy()
+    # dst_pts[:, 0], dst_pts[:, 1] = dst_pts[:, 1].copy(), dst_pts[:, 0].copy()
+
     src_pts = np.float32(np.array([[tl_c[1], tl_c[0]], [tr_c[1], tr_c[0]], [br_c[1], br_c[0]], [bl_c[1], bl_c[0]]]))
-    dst_pts = np.float32(np.array([[0, 0], [dst_w, 0], [dst_w, dst_h], [0, dst_h]]))
+    # dst_pts = np.float32(np.array([[0, 0], [dst_w, 0], [dst_w, dst_h], [0, dst_h]]))
+    dst_pts = np.float32(np.array([[0, dst_h], [dst_w, dst_h], [dst_w, 0], [0, 0]]))
+
+    # src_pts[:, 0], src_pts[:, 1] = src_pts[:, 1].copy(), src_pts[:, 0].copy()
+    # dst_pts[:, 0], dst_pts[:, 1] = dst_pts[:, 1].copy(), dst_pts[:, 0].copy()
+
+    print src_pts
+    print dst_pts
 
     A = cv2.getAffineTransform(src_pts[0:3], dst_pts[0:3])
+    # A = np.array([[1, 0, 30], [0, 1, 50]], dtype=np.float32)
 
-    im_ = np.zeros_like(crop)
-    im_[r.pts()[:, 0], r.pts()[:, 1]] = crop[r.pts()[:, 0], r.pts()[:, 1]]
+    crop = np.asarray(crop*255, dtype=np.uint8)
+    # im2 = cv2.warpAffine(crop, A, (dst_w, dst_h))
+    A[0, 2] = dst_h
+    A[1, 2] = dst_w
 
-    im2 = cv2.warpAffine(im_, A, (dst_w, dst_h))
+    im2 = cv2.warpAffine(crop, A, (1000, 1000))
 
     return im2
 
@@ -94,101 +119,56 @@ if __name__ == "__main__":
 
     p = Project()
     p.working_directory = '/Users/flipajs/Documents/wd/c4'
-    p.video_paths = ['/Users/flipajs/Documents/wd/c4_0h30m-0h33r.avi']
+    p.video_paths = ['/Users/flipajs/Documents/wd/c4_0h30m-0h33m.avi']
     # p.load('/Users/flipajs/Documents/wd/c4/c4.fproj')
 
     # solver = p.saved_progress['solver']
     solver = None
-    get_regions(p, solver, 0, 500)
+    regions = get_regions(p, solver, 0, 500)
 
     vid = get_auto_video_manager(p)
 
-    im = vid.next_frame()
-    # crop = im[746:801, 540:611, :]
-    # image = color.rgb2gray(crop)
+    chunk_id = 3
+    for f in range(0, 100, 10):
+        print f
+        im = vid.get_frame(f)
+        gray = color.rgb2gray(im)
 
-    # m = get_mser(image, p)
-    # crop = draw_points(crop, r.pts())
+        reg = None
+        for r in regions[f]:
+            if r['chunk_id'] == chunk_id:
+                reg = r['region']
 
-    # p_ = np.array([r.a_*math.sin(-r.theta_), r.a_*math.cos(-r.theta_)])
-    # head = np.ceil(r.centroid() + p_)
-    # back = np.ceil(r.centroid() - p_)
-    # print head, back
-    # crop[r.centroid_[0], r.centroid_[1]] = (255, 255, 0)
+        im_ = warp_region(reg, gray)
+        # im_ = np.asarray(im_*255, dtype=np.uint8)
+        cv2.imshow('im_', im_)
+        cv2.moveWindow('im_', 0, 0)
+        cv2.waitKey(0)
 
-    # dst_h = 8*2
-    # dst_w = 8*6
 
-    # head += np.array([1, 1])
-    # back -= np.array([1, 1])
 
-    # crop[head[0], head[1], :] = (0, 0, 255)
-    # crop[back[0], back[1], :] = (0, 255, 0)
-
-    # b_ = r.b_*2.5
-    # p_ = np.array([b_*math.sin(-r.theta_+np.pi+np.pi/2), b_*math.cos(-r.theta_+np.pi+np.pi/2)])
-    # tl_c = back + p_
-    # tr_c = head + p_
-    # bl_c = back - p_
-    # br_c = head - p_
-    # crop[tl_c[0], tl_c[1], :] = (255, 0, 0)
-    # crop[tr_c[0], tr_c[1], :] = (255, 0, 0)
-    # crop[bl_c[0], bl_c[1], :] = (255, 0, 0)
-    # crop[br_c[0], br_c[1], :] = (255, 0, 0)
-
-    if True:
-        # src_pts = np.float32(np.array([tl_c, tr_c, br_c, bl_c]))
-        # # srcP = np.float32(np.array([[0, 0], [dst_h, 0], [dst_h, dst_w], [0, dst_w]]))
-        # dst_pts = np.float32(np.array([[0, 0], [0, dst_w], [dst_h, dst_w], [dst_h, 0]]))
-        # # dstP = np.float32(np.array([[-30, 50], [m-50, -50], [m+80, n-100], [150, n+50]]))
-
-        # src_pts[:, 0], src_pts[:, 1] = src_pts[:, 1].copy(), src_pts[:, 0].copy()
-        # dst_pts[:, 0], dst_pts[:, 1] = dst_pts[:, 1].copy(), dst_pts[:, 0].copy()
-
-        # H = cv2.findHomography(dst_pts, dst_pts, 0)
-
-        # A = cv2.getAffineTransform(src_pts[0:3], dst_pts[0:3])
-        # P = cv2.getPerspectiveTransform(dst_pts, src_pts)
-
-        # tl_c = np.array([tl_c[0], tl_c[1], 1])
-        # print np.dot(A, tl_c.T)
-
-        # im2 = cv2.warpAffine(crop, H[0][0:2, :], (200, 200))
-        # im_ = np.zeros_like(image)
-        # # TODO: speed up...
-        # # for pt in r.pts():
-        # #     im_[pt[0], pt[1]] = image[pt[0], pt[1]]
-        # im_[r.pts()[:, 0], r.pts()[:, 1]] = image[r.pts()[:, 0], r.pts()[:, 1]]
-        #
-        # # image = np.ma.masked_array(image, mask=r.pts())
-        #
-        # im2 = cv2.warpAffine(im_, A, (dst_w, dst_h))
-        # im2 = cv2.warpPerspective(crop, P, (200, 200))
-
-        # new_tl = np.dot(tl_c, A)
-        # print new_tl
-        # im2 = im2[tl_c[0]:tl_c[0]+dst_h+1, tl_c[1]:tl_c[1]+dst_w+1]
-
-    fd, hog_image = hog(im2, orientations=9, pixels_per_cell=(8, 8),
-                        cells_per_block=(1, 1), visualise=True)
-
-    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 4))
-
-    ax1.axis('off')
-    ax1.imshow(crop, cmap=plt.cr.gray)
-    ax1.set_title('Input image')
-
-    ax2.axis('off')
-    ax2.imshow(im2, cmap=plt.cr.gray)
-    ax2.set_title('warped')
-
-    # Rescale histogram for better display
-    hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
-
-    ax3.axis('off')
-    ax3.imshow(hog_image_rescaled, cmap=plt.cr.gray)
-    ax3.set_title('Histogram of Oriented Gradients')
-    plt.show()
+    # im = vid.next_frame()
+    #
+    # fd, hog_image = hog(im2, orientations=9, pixels_per_cell=(8, 8),
+    #                     cells_per_block=(1, 1), visualise=True)
+    #
+    # fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8, 4))
+    #
+    # ax1.axis('off')
+    # ax1.imshow(crop, cmap=plt.cr.gray)
+    # ax1.set_title('Input image')
+    #
+    # ax2.axis('off')
+    # ax2.imshow(im2, cmap=plt.cr.gray)
+    # ax2.set_title('warped')
+    #
+    # # Rescale histogram for better display
+    # hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
+    #
+    # ax3.axis('off')
+    # ax3.imshow(hog_image_rescaled, cmap=plt.cr.gray)
+    # ax3.set_title('Histogram of Oriented Gradients')
+    # plt.show()
 
     app.exec_()
     app.deleteLater()
