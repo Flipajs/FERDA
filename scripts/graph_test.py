b54811c94e8825dd8af14864caca9cba7cb56022
Dita Hollmannova <holl.dita@gmail.com>
2015-09-11 14:56:39 +0200

    added comparison to networkx and some stats with graph creation times

--- scripts/graph_test.py
+++ scripts/graph_test.py
@@ -1,7 +1,28 @@
-import networkx
-import random
+import networkx as nt
 import graph_tool.all as gt
-import igraph
+import igraph as ig
+import numpy as np
+import random
+
+"""
+               graph-tool                                                igraph                                           networkx
+docs:          https://graph-tool.skewed.de/static/doc/graph_tool.html   http://igraph.org/python/doc/igraph-module.html
+pyqt:          No (cairo interactive popup)                              No (low quality image output)
+new graph:     g = Graph(directed=True)                                  g = Graph(directed=True)
+new edge:      g.add_edge(Vertex from, Vertex to)                        g.add_edge(int id1, int id2)
+new node:      g.add_vertex()                                            g.add_vertex()
+
+random graph creation without visualisation (results are in seconds, average from at least 5 measurings):
+600 vertices   0.017                                                     0.03                                             0.0047
+1000 vertices  0.027                                                     0.077                                            0.0055
+4000 vertices  0.12                                                      1.1                                              0.028
+20000 vertices 0.57                                                      26.6                                             0.25
+"""
+
+# TODO: check the remove_vertex and find_vertex functions in graph-tool
+
+
+
 
 
 class Node():
@@ -11,14 +32,40 @@ class Node():
         self.j = j
 
 
-def create_igraph(nodes, lenght):
-    g = igraph.Graph()
+def create_networkx(nodes, lenght):
+    g = nt.DiGraph()
     frames = []
     id = 0
     for i in range(0, lenght):
         n = []
         for j in range(0, nodes):
-            n.append(Node(id, i, j))
+            n.append(id)
+            g.add_node(id)
+            id += 1
+        frames.append(n)
+
+    print "Nodes are ready"
+
+    vertices = []
+    id = 0
+    for i in range(0, lenght-1):
+        for j in range(0, nodes):
+            d1 = random.randint(1, nodes-1)
+            vertices.append(g.add_edge(frames[i][j], frames[i+1][d1]))
+            id += 1
+
+    print "Graph complete!"
+    return g
+
+
+def create_igraph(nodes, lenght):
+    g = ig.Graph(directed=True)
+    frames = []
+    id = 0
+    for i in range(0, lenght):
+        n = []
+        for j in range(0, nodes):
+            n.append(id)
             g.add_vertex(id, shape='hidden')
             id += 1
         frames.append(n)
@@ -29,18 +76,14 @@ def create_igraph(nodes, lenght):
     id = 0
     for i in range(0, lenght-1):
         for j in range(0, nodes):
-            n1 = frames[i][j]
             d1 = random.randint(1, nodes-1)
-            n2 = frames[i+1][d1]
-            vertices.append(g.add_edge(n1.id, n2.id))
+            vertices.append(g.add_edge(frames[i][j], frames[i+1][d1]))
             id += 1
 
     print "Graph complete!"
-    import time
-    t = time.time()
-    layout = g.layout("rt")
-    igraph.plot(g, layout = layout, )
-    print time.time()-t
+    #layout = g.layout("rt")
+    #ig.plot(g, layout = layout, )
+    return g
 
 def create_graph_tool(nodes, lenght):
     g = gt.Graph(directed=True)
@@ -66,12 +109,28 @@ def create_graph_tool(nodes, lenght):
             id += 1
 
     print "Graph complete!"
-    import time
-    t = time.time()
-    gt.graph_draw(g)
-    print time.time()-t
+    #gt.graph_draw(g)
+    return g
+
+def randoms(array):
+    for i in range(0, len(array)):
+        yield array[i]
 
 
+import time
+t = time.time()
+igraph = create_igraph(2, 100)
+print "igraph: %s" % (time.time()-t)
+"""
+t = time.time()
+graph_tool = create_graph_tool(2, 10000)
+print "graph-tool: %s" % (time.time()-t)
+"""
+t = time.time()
+networkx = create_networkx(2, 10000)
+print "networkx: %s" % (time.time()-t)
 
-create_igraph(10, 30)
-#create_graph_tool(10, 300)
+
+# 100 random numbers from range 0 to 100
+rnd_ints = np.random.rand(0, 100, 100)
+igraph.delete_edges(randoms(rnd_ints))
