01695e94eb331a53f400e9d6081db460d5439c03
Filip Naiser <filip@naiser.cz>
2015-09-21 18:10:52 +0200

    toggle bug in global view fixed

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ project = Project()
 
 S_.general.print_log = False
 
-project.load('/home/dita/PycharmProjects/c5__/c5__.fproj')
-ex.widget_control('load_project', project)
+# project.load('/home/dita/PycharmProjects/c5__/c5__.fproj')
+# ex.widget_control('load_project', project)
 # This is development speed up process (kind of fast start). Runs only on developers machines...
 if is_flipajs_pc():
     # project.load('/Users/flipajs/Documents/wd/F1C51/f1c51.fproj')
@@ -25,9 +25,9 @@ if is_flipajs_pc():
     # project.load('/Users/flipajs/Documents/wd/small_lenses_colony1/small_lenses    .fproj')
     # project.load('/Users/flipajs/Documents/wd/colonies1_30m/colonies30m.fproj')
     # project.load('/Users/flipajs/Documents/wd/eight/eight.fproj')
-    # project.load('/Users/flipajs/Documents/wd/eight/eight.fproj')
+    project.load('/Users/flipajs/Documents/wd/c4/c4.fproj')
     # # # # # #
-    # ex.widget_control('load_project', project)
+    ex.widget_control('load_project', project)
 
     pass
 
