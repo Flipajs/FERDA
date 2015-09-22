4ebf74f7b3b59699002f0a56354816efda86457a
Filip Naiser <filip@naiser.cz>
2015-09-22 11:44:29 +0200

    created skeleton for chunk_manager

---
+++ core/graph/chunk_manager.py
@@ -0,0 +1,22 @@
+__author__ = 'flipajs'
+
+import chunk
+
+
+class ChunkManager:
+    def __init__(self):
+        self.id = 0
+        self.chunks_ = {}
+        pass
+
+    # TODO:
+    # https://docs.python.org/2/reference/datamodel.html#emulating-container-types
+    # at least slice access
+
+    def __getitem__(self, index):
+        return self.chunks_.get(index, None)
+
+    def new_chunk(self):
+        # Chunk(start_n=None, end_n=None, solver=None, store_area=False, id=-1)
+        # we have to provide also nm - NodeManager
+        pass
