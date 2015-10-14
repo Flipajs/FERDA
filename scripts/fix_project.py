c151447e2c1f65dfa39c5daa3c3b341087042587
Simon Mandlik <simon.mandlik@gmail.com>
2015-10-14 11:13:40 +0200

    ...

--- scripts/fix_project.py
+++ scripts/fix_project.py
@@ -4,12 +4,15 @@ from core.project.project import Project
 
 
 p = Project()
-p.load('/Users/flipajs/Documents/wd/eight_barbara_bug/eight.fproj')
+p.load('/home/sheemon/FERDA/projects/eight_new/eight.fproj')
 
-p.video_paths = ['/Users/flipajs/Documents/wd/eight.m4v']
-p.working_directory = '/Users/flipajs/Documents/wd/eight_barbara_bug'
+p.video_paths = ['/home/sheemon/FERDA/projects/eight.m4v']
+p.working_directory = '/home/sheemon/FERDA/projects/eight_new'
 
 for it in p.log.data_:
     print it.action_name, it.data
 
 p.save()
+
+if __name__ == "__main__":
+    execfile("/home/sheemon/FERDA/ferda/scripts/fix_project.py")
