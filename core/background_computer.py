2188b60faaeaffe8d2230e6f449a9b64d855f314
Barbara Casillas <casillas@ist.ac.at>
2015-10-23 17:07:40 +0200

    changes Barbara C

--- core/background_computer.py
+++ core/background_computer.py
@@ -66,6 +66,7 @@ class BackgroundComputer():
             for i in range(skip_n_first_parts):
                 self.processes.append(None)
 
+            limitsFile = open(str(self.project.working_directory)+"/limits.txt","w");
             for i in range(skip_n_first_parts, self.part_num):
                 p = QtCore.QProcess()
 
@@ -81,16 +82,20 @@ class BackgroundComputer():
 
                 ex_str = str(sys.executable) + ' "'+os.getcwd()+'/core/parallelization.py" "'+ str(self.project.working_directory)+'" "'+str(self.project.name)+'" '+str(i)+' '+str(f_num)+' '+str(last_n_frames)
                 print ex_str
+                limitsFile.write(str(i)+" "+str(f_num)+" "+str(last_n_frames)+"\n");
                 status = self.WAITING
                 if i < skip_n_first_parts + self.process_n:
                     status = self.RUNNING
-                    p.start(str(sys.executable) + ' "'+os.getcwd()+'/core/parallelization.py" "'+ str(self.project.working_directory)+'" "'+str(self.project.name)+'" '+str(i)+' '+str(f_num)+' '+str(last_n_frames))
+                    #p.start(str(sys.executable) + ' "'+os.getcwd()+'/core/parallelization.py" "'+ str(self.project.working_directory)+'" "'+str(self.project.name)+'" '+str(i)+' '+str(f_num)+' '+str(last_n_frames))   ## Uncomment for cluster usage
 
                 self.processes.append([p, ex_str, status])
 
                 # self.update_callback('DONE: '+str(i+1)+' out of '+str(self.process_n))
 
             S_.general.log_graph_edits = True
+            limitsFile.close();
+            sys.exit() ## Comment for cluster usage
+            
         else:
             self.precomputed = True
 
--- scripts/fix_project.py
+++ scripts/fix_project.py
@@ -1,13 +1,14 @@
-__author__ = 'flipajs'
+l__author__ = 'flipajs'
 
 from core.project.project import Project
 
 
 p = Project()
-p.load('/Users/flipajs/Documents/wd/eight_barbara_bug/eight.fproj')
+#p.load('/Users/flipajs/Documents/wd/eight_barbara_b/eight.fproj')
+p.load('/home/casillas/Documents/FERDAWD/test3/test.fproj')
 
-p.video_paths = ['/Users/flipajs/Documents/wd/eight.m4v']
-p.working_directory = '/Users/flipajs/Documents/wd/eight_barbara_bug'
+p.video_paths = ['/home/casillas/Documents/FERDAWD/eight.m4v']
+p.working_directory = '/home/casillas/Documents/FERDAWD/'
 
 for it in p.log.data_:
     print it.action_name, it.data
