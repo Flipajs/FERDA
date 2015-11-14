found bug and finally colormarks are working. It is important to do the normalisation on whole image! Not only on cropped area (which make sense)...

core/colormarks/analyse_chunk.py
@@ -4,6 +4,11 @@ from core.graph.region_chunk import RegionChunk
 from utils.img import get_safe_selection
 from math import ceil
 from processing import get_colormarks, match_cms_region
+import matplotlib.pyplot as plt
+import numpy as np
+from scripts.irg_hist_demo import ColorHist3d
+from scripts.irg_hist_demo import *
+from processing import transform_img_
 
 
 def analyse_chunk(ch, project, cm_model, sample_step):
@@ -14,19 +19,17 @@ def analyse_chunk(ch, project, cm_model, sample_step):
         r = r_ch[t - r_ch.start_frame()]
 
         # TODO: set some reasonable parameters
-        bb = get_bounding_box(r, project)
+        bb = get_bounding_box(r, project, cm_model)
 
         cms = get_colormarks(bb, cm_model)
         for pts, label in cms:
             print label
             for pt in pts:
-                if label == 6:
-                    bb[pt[0], pt[1], 0] = 255
-                else:
-                    bb[pt[0], pt[1], 1] = 255
+                bb[pt[0], pt[1], 0] = 255
 
         cv2.imshow('bb', bb)
-        cv2.waitKey(25)
+        cv2.imshow('bb_t', transform_img_(bb, cm_model))
+        cv2.waitKey(0)
         matches = match_cms_region(cms, r)
 
         ch_cms[t] = matches
@@ -34,13 +37,14 @@ def analyse_chunk(ch, project, cm_model, sample_step):
     return ch_cms
 
 
-def get_bounding_box(r, project):
+def get_bounding_box(r, project, cm_model):
     # TODO set this...:
     border_percent = 1.3
 
     frame = r.frame()
     img = project.img_manager.get_whole_img(frame)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = transform_img_(img, cm_model)
 
     roi = r.roi()
 
@@ -74,24 +78,26 @@ if __name__ == '__main__':
 
     from utils.video_manager import get_auto_video_manager
     vm = get_auto_video_manager(p)
-    main_img = vm.get_frame(500)
+    frame = 0
+    main_img = vm.get_frame(frame)
     main_img = cv2.cvtColor(main_img, cv2.COLOR_BGR2RGB)
-
-    for frame, mask in masks:
-        im = vm.get_frame(frame)
-        # ccs = get_ccs(mask)
-        cv2.imshow('mask', mask)
-        cv2.waitKey(0)
-        # im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-        # irg_255 = get_irg_255(im)
-        # sample_pxs, all_pxs = find_dist_thresholds(ccs, irg_255.copy())
-
-        # color_samples.append((sample_pxs, all_pxs))
 
 
     cm_model.compute_model(main_img, color_samples)
 
+    for px in color_samples[0][0]:
+        pos = np.asarray(px / cm_model.num_bins_v, dtype=np.int)
+        print px, cm_model.hist3d.hist_labels_[pos[0], pos[1], pos[2]]
 
+    test_im = np.zeros((50, 50, 3), dtype=np.uint8)
+    i = 0
+    j = 0
+    for px in color_samples[0][1]:
+        test_im[i, j, :] = px
+        j += 1
+        if j == 20:
+            i += 1
+            j = 0
 
     chunks = []
 
@@ -99,7 +105,6 @@ if __name__ == '__main__':
         ch_id = p.gm.g.vp['chunk_start_id'][p.gm.g.vertex(v_id)]
         if ch_id > 0:
             chunks.append(p.chm[ch_id])
-
 
     # TODO: remove, debug...
     measurements = analyse_chunk(chunks[7], p, cm_model, 3)

core/colormarks/colormarks_model.py
@@ -11,6 +11,7 @@ class ColormarksModel:
         self.im_space = 'irb'
         self.hist3d = None
         self.num_colors = -1
+        self.num_bins_v = None
 
     def compute_model(self, main_img, color_samples):
         self.num_bins_v = np.array([self.num_bins1, self.num_bins2, self.num_bins3], dtype=np.float)
@@ -23,7 +24,7 @@ class ColormarksModel:
                                   num_bins1=self.num_bins1, num_bins2=self.num_bins2, num_bins3=self.num_bins3,
                                   theta=0.3, epsilon=0.9)
 
-        for (picked_pxs, all_pxs), c_id in zip(color_samples, range(len(color_samples))):
+        for (picked_pxs, all_pxs), c_id in zip(color_samples, range(1, len(color_samples)+1)):
             self.hist3d.remove_bg(all_pxs)
             self.hist3d.add_color(picked_pxs, c_id)
 

core/colormarks/hist_3d.py
@@ -14,7 +14,7 @@ class ColorHist3d():
 
         self.num_pxs = im.shape[0] * im.shape[1] * im.shape[2]
         self.num_colors = num_colors
-        self.BG = num_colors
+        self.BG = 0
 
         pos = np.asarray(im / self.num_bins_v, dtype=np.int)
 
@@ -80,7 +80,8 @@ class ColorHist3d():
         return a / float(n)
 
     def assign_labels(self):
-        for c_id in range(self.num_colors):
+        # skip bg
+        for c_id in range(1, self.num_colors+1):
             sum_ = 0
             good_enough = []
 

core/colormarks/processing.py
@@ -3,13 +3,14 @@ from scipy import ndimage
 
 
 def get_colormarks(img, cm_model):
-    img_t = transform_img_(img, cm_model)
+    # img_t = transform_img_(img, cm_model)
+    # img_t = img
 
-    pos = np.asarray(img_t / cm_model.num_bins_v, dtype=np.int)
+    pos = np.asarray(img / cm_model.num_bins_v, dtype=np.int)
     labels = cm_model.get_labelling(pos)
 
     # TOOD: parameters
-    ccs = get_ccs_(labels, bg=-1, max_a=100000)
+    ccs = get_ccs_(labels, bg=0, max_a=2000)
     return ccs
 
 
@@ -22,7 +23,7 @@ def get_ccs_(im, bg=-1, min_a=1, max_a=5000):
 
     labeled, num = skimage.measure.label(im, background=bg, return_num=True)
 
-    sizes = ndimage.sum(np.ones((im.shape[0], im.shape[1])), labeled, range(1, num + 1))
+    # sizes = ndimage.sum(np.ones((im.shape[0], im.shape[1])), labeled, range(1, num + 1))
 
     ccs = []
     for i in range(num):

scripts/irg_hist_demo.py
@@ -266,9 +266,9 @@ def show_foreground(CH3d, data, im):
               [55, 255, 255], [255, 255, 0],  # Blue Yellow
               [255, 107, 151], [0, 0, 0]]  # Pink Black
 
-    colors = [[255, 255, 255], [55, 255, 255],  # white blue
-              [0, 255, 0], [255, 255, 0],  # green yellow
-              [255, 107, 151], [50, 50, 50]]  # pink bg
+    # colors = [[255, 255, 255], [55, 255, 255],  # white blue
+    #           [0, 255, 0], [255, 255, 0],  # green yellow
+    #           [255, 107, 151], [50, 50, 50]]  # pink bg
 
     # colors = [[255, 127, 166], [255, 255, 255], [255, 253, 22],
     #           [0, 255, 57], [0, 189, 255], [255, 255, 0],
@@ -313,39 +313,40 @@ def get_irg_255(im):
     return irg_255
 
 
-def get_color_samples_tool(vid):
+def get_color_samples_tool(vid, frames=None, wd_=''):
     global wd
 
     name = 'color_samples'
 
     color_samples = []
     masks = []
-    try:
-        with open(wd + '/' + name + '.pkl', 'rb') as f:
-            up = pickle.Unpickler(f)
-            color_samples = up.load()
-            masks = up.load()
-    except IOError:
-        pass
-    except EOFError:
-        pass
+    # try:
+    #     with open(wd + '/' + name + '.pkl', 'rb') as f:
+    #         up = pickle.Unpickler(f)
+    #         color_samples = up.load()
+    #         masks = up.load()
+    # except IOError:
+    #     pass
+    # except EOFError:
+    #     pass
 
-    color_samples = []
-    for frame, mask in masks:
-        im = vid.get_frame(frame)
-        ccs = get_ccs(mask)
+    # color_samples = []
+    # for frame, mask in masks:
+    #     im = vid.get_frame(frame)
+    #     ccs = get_ccs(mask)
+    #
+    #     im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    #     irg_255 = get_irg_255(im)
+    #     sample_pxs, all_pxs = find_dist_thresholds(ccs, irg_255.copy())
+    #
+    #     color_samples.append((sample_pxs, all_pxs))
 
-        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-        irg_255 = get_irg_255(im)
-        sample_pxs, all_pxs = find_dist_thresholds(ccs, irg_255.copy())
-
-        color_samples.append((sample_pxs, all_pxs))
-
-    frames = [500, 500, 500, 500, 500, 300]
-    f = 916
-    frames = [f, f, f, f, f, f, f, f, f]
-    # frames = [52, 52, 52, 52, 52]
-    frames = []
+    if not frames:
+        frames = [500, 500, 500, 500, 500, 300]
+        # f = 916
+        # frames = [f, f, f, f, f, f, f, f, f]
+        # frames = [52, 52, 52, 52, 52]
+        # frames = []
 
     app = QtGui.QApplication(sys.argv)
 
@@ -380,7 +381,7 @@ def get_color_samples_tool(vid):
 
     app.deleteLater()
 
-    with open(wd + '/' + name + '.pkl', 'wb') as f:
+    with open(wd_ + '/' + name + '.pkl', 'wb') as f:
         p_ = pickle.Pickler(f, -1)
         p_.dump(color_samples)
         p_.dump(masks)
@@ -414,10 +415,10 @@ if __name__ == "__main__":
 
     num_bins_v = np.array([NUM_BINS1, NUM_BINS2, NUM_BINS3], dtype=np.float)
 
-    wd = '/Users/flipajs/Documents/wd/blc2-test'
+    wd = '/Users/flipajs/Documents/wd/C210'
 
-    # vid = VideoManager('/Users/flipajs/Documents/wd/C210min.avi')
-    vid = VideoManager('/Volumes/Seagate Expansion Drive/IST - videos/bigLenses_colormarks2.avi')
+    vid = VideoManager('/Users/flipajs/Documents/wd/C210min.avi')
+    # vid = VideoManager('/Volumes/Seagate Expansion Drive/IST - videos/bigLenses_colormarks2.avi')
     # vid = VideoManager('/Users/flipajs/Documents/wd/bigLense_clip.avi')
 
     color_samples = get_color_samples_tool(vid)
@@ -428,9 +429,9 @@ if __name__ == "__main__":
 
     if True:
         im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-        if False:
+        if True:
             irg_255 = get_irg_255(im)
-            CH3d = ColorHist3d(irg_255.copy(), 5, num_bins1=NUM_BINS1, num_bins2=NUM_BINS2, num_bins3=NUM_BINS3,
+            CH3d = ColorHist3d(irg_255.copy(), 6, num_bins1=NUM_BINS1, num_bins2=NUM_BINS2, num_bins3=NUM_BINS3,
                                theta=0.3, epsilon=0.9)
 
             for (picked_pxs, all_pxs), c_id in zip(color_samples, range(len(color_samples))):