f8d4b4f39abbf0c52d6909352f3b419aa513f9b0
Filip Naiser <filip@naiser.cz>
2016-03-09 12:45:31 +0100

    fixed load project bug

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ project = Project()
 S_.general.print_log = False
 
 # This is development speed up process (kind of fast start). Runs only on developers machines...
-if is_flipajs_pc():
-    sn_id = 2
-    name = 'Cam1_orig'
-    wd = '/Users/flipajs/Documents/wd/GT/'
-    snapshot = {'chm': wd+name+'/.auto_save/'+str(sn_id)+'__chunk_manager.pkl',
-                'gm': wd+name+'/.auto_save/'+str(sn_id)+'__graph_manager.pkl'}
-
-    project.load(wd+name+'/cam1.fproj')
-    ex.widget_control('load_project', project)
+# if is_flipajs_pc():
+#     sn_id = 2
+#     name = 'Cam1_orig'
+#     wd = '/Users/flipajs/Documents/wd/GT/'
+#     snapshot = {'chm': wd+name+'/.auto_save/'+str(sn_id)+'__chunk_manager.pkl',
+#                 'gm': wd+name+'/.auto_save/'+str(sn_id)+'__graph_manager.pkl'}
+#
+#     project.load(wd+name+'/cam1.fproj')
+#     ex.widget_control('load_project', project)
 
 
 app.exec_()
