589db480d88b7d37e274270d183b30078f4a6c89
Simon Mandlik <simon.mandlik@gmail.com>
2016-03-17 16:46:07 +0100

    ...

---
+++ core/region/position_recognition/position_recognition_test.py
@@ -0,0 +1,31 @@
+from core.project.project import Project
+
+project = Project()
+project.load("/home/sheemon/FERDA/projects/Cam1_/cam1.fproj")
+
+def get_chunks_regions(chunk):
+    chunk_start = chunk.start_frame(project.gm)
+    chunk_end = chunk.end_frame(project.gm)
+    while chunk_start <= chunk_end:
+        yield project.gm.region(chunk[chunk_start])
+        chunk_start += 1
+
+
+def get_matrix(chunk):
+    matrix = []
+    for region in get_chunks_regions(chunk):
+        matrix.append(get_region_vector(region))
+    return matrix
+
+
+def get_region_vector(region):
+    vector = []
+    print(region.contour())
+    print(region.centroid())
+    return vector
+
+
+if __name__ == '__main__':
+
+    chunks = project.gm.chunk_list()
+    print(get_matrix(chunks[0]))
