cc14d3473a5e7f234edc61cd8d3d98d841d4729b
Simon Mandlik <simon.mandlik@gmail.com>
2016-04-07 17:46:03 +0200

    first tries

--- core/region/position_recognition/position_recognition_test.py
+++ core/region/position_recognition/position_recognition_test.py
@@ -1,8 +1,37 @@
+import numpy
+from numpy import transpose
 from core.project.project import Project
+from numpy.linalg import eig
+import math
+
+NUMBER_OF_DATA = 10
+PRECISION = 3
 
 project = Project()
 project.load("/home/sheemon/FERDA/projects/Cam1_/cam1.fproj")
 
+
+def get_pca(chunks):
+    matrix = []
+    for ch in chunks:
+        for vector in get_matrix(ch):
+            matrix.append(vector)
+    # for a in matrix:
+    #     print (a)
+    eigenfaces = get_eigenfaces(matrix)
+    return numpy.dot(transpose(eigenfaces[:PRECISION]), matrix)
+
+
+def get_eigenfaces(chunk_matrix):
+    vector_matrix = numpy.array(chunk_matrix)
+    transposed = transpose(chunk_matrix)
+    # with open("out.txt", mode="w") as file:
+    #     file.write(str(vector_matrix))
+    #     file.write("\n\n\n")
+    #     file.write(str(transposed))
+    covariance_matrix = numpy.dot(transposed, chunk_matrix)
+    eigens = eig(covariance_matrix)
+    return eigens
 
 def get_chunks_regions(ch):
     chunk = project.chm[ch]
@@ -22,11 +51,50 @@ def get_matrix(chunk):
 
 def get_region_vector(region):
     vector = []
-    print(region.contour())
-    print(region.centroid())
+    centroid = region.centroid()
+    contours = region.contour()
+    con_index = 0
+    head_beam = centroid - contours[0]
+    last = head_beam
+    last_angle = 0
+    angle = 0
+    circle = math.pi * 2
+    while angle < circle:
+        beam = centroid - contours[con_index]
+        ang = deviation(head_beam, beam)
+        if ang == angle:
+            vector.append(vector_norm(beam))
+        elif ang > angle:
+            vector.append(interpolation(angle - last_angle, ang - angle, last, beam))
+        last = beam
+        last_angle = ang
+        con_index += 1
+        angle += circle / NUMBER_OF_DATA
+    # hlavicka?
     return vector
+
+
+def vector_norm(u):
+    return math.sqrt(sum(i ** 2 for i in u))
+
+
+def dot_product(u, v):
+    return sum((x * y) for x, y in zip(u, v))
+
+
+def deviation(u, v):
+    return math.acos(dot_product(u, v) / (vector_norm(u) * vector_norm(v)))
+
+
+def interpolation(first_angle, sec_angle, u, v):
+    size_u = vector_norm(u)
+    size_v = vector_norm(v)
+    if size_u > size_v:
+        size_u, size_v = size_v, size_u
+        first_angle, sec_angle = sec_angle, first_angle
+    return size_u + (size_v - size_u) / 2 * (first_angle / sec_angle)
 
 
 if __name__ == '__main__':
     chunks = project.gm.chunk_list()
-    print(get_matrix(chunks[0]))
+    get_pca(chunks[:1])
