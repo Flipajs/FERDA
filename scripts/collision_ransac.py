42684318cc0c5d4ad6466fedb739c82a95cddeea
Filip Naiser <filip@naiser.cz>
2016-05-06 13:29:39 +0200

    before global optimisation attempt

--- scripts/collision_ransac.py
+++ scripts/collision_ransac.py
@@ -12,11 +12,15 @@ from scipy.spatial.distance import cdist
 
 def data_cam2():
     #Cam2
-    collisions = [
-        {'s': [6, 7], 'm': 1, 'e': [18, 23]},
-        {'s': [64, 65], 'm': 50, 'e': [48, 62]},
-        {'s': [111, 112, 120], 'm': 132, 'e': [123, 124, 116]},
-                  ]
+    collisions = {
+        1: {'s': [6, 7], 'm': 1, 'e': [18, 23]},
+        2: {'s': [64, 65], 'm': 50, 'e': [48, 62]},
+        3: {'s': [111, 112, 120], 'm': 132, 'e': [123, 124, 116]},
+        394: {'s': [60, 61], 'm': 49, 'e': [77, 78]},
+        1985: {'s': [306, 307], 'm': 302, 'e': [297, 305]},
+        3130: {'s': [425, 411], 'm': 420, 'e': [421, 422]},
+        3350: {'s': [464, 460], 'm': 457, 'e': [452, 467]}
+    }
 
     return collisions
 
@@ -65,23 +69,35 @@ def get_geom_s(start_, coef, num):
 
     return r
 
-def __get_support(pts1, p_type_starts1, pts2, p_type_starts2, type_weights, r, t, rot_center, thresh=1):
+def __get_support(pts1, p_type_starts1, pts2, p_type_starts2, type_weights, r, t, rot_center, intensities=None, thresh=1):
     pts1 =__transform_pts(pts1, r, t, rot_center)
 
     # threshs = get_geom_s(2, 2**0.5, 5)
     supp = 0
     # -1 because, there is last number describing the end of interval in p_type_starts...
     for c in range(len(p_type_starts1)-1):
-        d = cdist(pts1[p_type_starts1[c]:p_type_starts1[c+1], :], pts2[p_type_starts2[c]:p_type_starts2[c+1], :])
+        ids1_ = slice(p_type_starts1[c], p_type_starts1[c+1])
+        ids2_ = slice(p_type_starts2[c], p_type_starts2[c+1])
+
+        d = cdist(pts1[ids1_, :], pts2[ids2_, :])
         mins_ = np.min(d, axis=1)
+        amins_ = np.argmin(d, axis=1)
 
+        mins_ = mins_**2
+        mins_[mins_ > thresh] = thresh
 
-        mins_ = mins_**2 - thresh**2
-        mins_[mins_ > 0] = 0
+        supp += np.sum(mins_) * type_weights[c]
+        if intensities is not None:
+            int2 = intensities[1][ids2_]
+            int_diff = intensities[0][ids1_] - int2[amins_]
+            int_diff = abs(np.asarray(int_diff[mins_ < thresh], dtype=np.float) / 10)
 
-        # supp += type_weights[c] * np.sum(mins_ < thresh)
-        supp += -np.sum(mins_) * type_weights[c]
-        # supp += np.sum(mins_ < threshs[c])
+            supp += sum(int_diff) * type_weights[c]
+
+        c = 50
+        if True:
+            supp += np.linalg.norm(t)**1.2 + c*r
+
 
     return supp
 
@@ -93,6 +109,7 @@ def estimate_rt(kps1, kps2):
     type_starts1 = []
     pts1 = []
     angles1 = []
+    intensities1 = []
     si = 0
 
     for a in sorted([int(x) for x in kps1]):
@@ -100,23 +117,28 @@ def estimate_rt(kps1, kps2):
         for b in kps1[a]:
             pts1.append(b['point'])
             angles1.append(b['angle'])
+            intensities1.append(b['intensity'])
             p_type1.append(si)
 
         si += 1
 
     type_starts1.append(len(pts1))
+    intensities1 = np.array(intensities1, dtype=np.int32)
 
     pts1 = np.array(pts1)
 
     type_starts2 = []
     pts2 = []
     angles2 = []
+    intensities2 = []
     for a in sorted([int(x) for x in kps2]):
         type_starts2.append(len(pts2))
         for b in kps2[a]:
             pts2.append(b['point'])
             angles2.append(b['angle'])
+            intensities2.append(b['intensity'])
 
+    intensities2 = np.array(intensities2, dtype=np.int32)
     type_starts2.append(len(pts2))
 
     pts2 = np.array(pts2)
@@ -127,7 +149,7 @@ def estimate_rt(kps1, kps2):
     best_t = []
     best_r = []
     best_rot_center = []
-    best_supp = [0]
+    best_supp = [np.inf]
 
     for i in range(steps):
         ai = random.randint(len(pts1), size=2)
@@ -144,10 +166,10 @@ def estimate_rt(kps1, kps2):
         # type_weights = [0.2, 0.35, 0.7, 1.3, 2]
         type_weights = [1, 2]
         # type_weights = [1, 1, 1, 1, 1]
-        supp = __get_support(pts1, type_starts1, pts2, type_starts2, type_weights, r, t, rot_center, thresh=5)
+        supp = __get_support(pts1, type_starts1, pts2, type_starts2, type_weights, r, t, rot_center, intensities=(intensities1, intensities2), thresh=5)
 
         i = 0
-        while i < len(best_supp) and supp > best_supp[i]:
+        while i < len(best_supp) and supp < best_supp[i]:
             i += 1
 
         if i > 0:
@@ -168,13 +190,15 @@ if __name__ == '__main__':
     p.load(wd+name)
     vm = get_auto_video_manager(p)
 
-    d = data[2]
+    d = data[3350]
 
     rs1 = p.gm.region(p.chm[d['s'][0]].end_vertex_id())
     rs2 = p.gm.region(p.chm[d['s'][1]].end_vertex_id())
-    rs3 = p.gm.region(p.chm[d['s'][2]].end_vertex_id())
+    # rs3 = p.gm.region(p.chm[d['s'][2]].end_vertex_id())
 
-    # im = vm.get_frame(rs1.frame())
+    im = vm.get_frame(rs1.frame())
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
     # plt.figure()
     # plt.imshow(im)
     # plt.show()
@@ -187,13 +211,17 @@ if __name__ == '__main__':
     # kps3 = get_curvature_kp(rs3.contour_without_holes(), True)
 
 
-    r = p.gm.region(p.chm[d['m']].start_vertex_id())
+    from core.graph.region_chunk import RegionChunk
+    rch = RegionChunk(p.chm[d['m']], p.gm, p.rm)
+
+    # r = p.gm.region(p.chm[d['m']].start_vertex_id())
+    r = rch[1]
     # kpsm = get_curvature_kp(r.contour_without_holes(), True)
 
 
     step = 5
 
-    rs__ = rs3
+    rs__ = rs2
     test1 = {0: [], 1:[]}
     pts1 = []
     pts__ = rs__.pts()
@@ -202,15 +230,18 @@ if __name__ == '__main__':
     for i in range(len(pts__)):
         # if i % step == 0:
             p = pts__[i, :]
-            test1[0].append({'point': p, 'angle': 0})
+            test1[0].append({'point': p, 'angle': 0, 'intensity': gray[p[0], p[1]]})
             pts1.append(p)
 
     pts__ = rs__.contour_without_holes()
     for i in range(len(pts__)):
         if i % step == 0:
             p = pts__[i, :]
-            test1[1].append({'point': p, 'angle': 0})
+            test1[1].append({'point': p, 'angle': 0, 'intensity': gray[p[0], p[1]]})
 
+
+    im = vm.get_frame(r.frame())
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
     pts1 = np.array(pts1)
 
@@ -221,23 +252,23 @@ if __name__ == '__main__':
     for i in range(len(pts__)):
         # if i % step == 0:
             p = pts__[i, :]
-            testm[0].append({'point': p, 'angle': 0})
+            testm[0].append({'point': p, 'angle': 0, 'intensity': gray[p[0], p[1]]})
             ptsm.append(p)
 
     pts__ = r.contour_without_holes()
     for i in range(len(pts__)):
         if i % step == 0:
             p = pts__[i, :]
-            testm[1].append({'point': p, 'angle': 0})
+            testm[1].append({'point': p, 'angle': 0, 'intensity': gray[p[0], p[1]]})
 
     ptsm = np.array(ptsm)
 
     best_t, best_r, best_rot_center, best_supp = estimate_rt(test1, testm)
 
     plt.figure()
-    plt.scatter(ptsm[:, 0], ptsm[:, 1], c='k', s=30, alpha=.70)
+    plt.scatter(ptsm[:, 1], ptsm[:, 0], c='k', s=30, alpha=.70)
     plt.hold(True)
-    plt.scatter(pts1[:, 0], pts1[:, 1], c='r', s=30, alpha=.70)
+    plt.scatter(pts1[:, 1], pts1[:, 0], c='r', s=30, alpha=.20)
 
     cs = ['g', 'b', 'c', 'm', 'k', 'w', 'y']
     # pts__ = []
@@ -250,8 +281,10 @@ if __name__ == '__main__':
         print best_supp[i+1]
         pts_ = __transform_pts(pts1, best_r[i], best_t[i], best_rot_center[i])
         plt.hold(True)
-        plt.scatter(pts_[:, 0], pts_[:, 1], c=cs[i%len(cs)], s=100, alpha=0.4)
+        plt.scatter(pts_[:, 1], pts_[:, 0], c=cs[i%len(cs)], s=100, alpha=0.4)
         plt.hold(False)
+
+        plt.axis('equal')
 
         plt.show()
         plt.waitforbuttonpress()
