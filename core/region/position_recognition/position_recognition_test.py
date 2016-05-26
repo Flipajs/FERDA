a6ac2c1effff9b93320d2f4ad2123fcfa52cbfb9
Simon Mandlik <simon.mandlik@gmail.com>
2016-05-26 16:58:12 +0200

    curve

--- core/region/position_recognition/position_recognition_test.py
+++ core/region/position_recognition/position_recognition_test.py
@@ -47,7 +47,7 @@ def get_chunks_regions(ch):
 def get_matrix(chunk, number_of_data):
     matrix = []
     for region in get_chunks_regions(chunk):
-        matrix.append(get_region_vector(region, number_of_data))
+        matrix.append(get_region_vector_curve(region, number_of_data))
     return matrix
 
 
@@ -71,69 +71,137 @@ def find_closest_angle_pos(beam_angle, number_of_data):
     return left
 
 
-def get_region_vector(region, number_of_data):
+def get_region_vector_curve(region, number_of_data):
+    centroid = region.centroid()
+    contour = region.contour()
+    size = len(contour)
+    if size < number_of_data:
+        print("Number of data should be smaller than contours length")
+    curve_length = compute_contour_perimeter(contour)
+    step = curve_length / number_of_data - 1
+    result = []
+    current = 0
+    length = 0
+    last = contour[size - 1]
+    points_aux = []
+    for i in range(size):
+        print(i)
+        curr = contour[i]
+        print(curr)
+        points_aux.append(curr)
+        diff = vector_norm(last - curr)
+        if length + diff > current * step:
+            result.append(get_interpolation_curve(vector_norm(last - centroid), vector_norm(curr - centroid),
+                                                       current * step - length, (length + diff) - current * step))
+            current += 1
+        length += diff
+        last = curr
+
+    plt.hold(True)
+    plt.axis('equal')
+    xs = [x[1] for x in points_aux]
+    ys = [x[0] for x in points_aux]
+    xa = [step * x for x in range(number_of_data)]
+    ya = [result[x] for x in range(number_of_data)]
+    plt.scatter(xs, ys, c="c")
+    plt.scatter(xa, ya)
+    plt.show(True)
+    return result
+
+
+def get_interpolation_curve(distance_a, distance_b, length_a, length_b):
+    return (distance_a * length_a + distance_b * length_b) / (length_a + length_b)
+
+def compute_contour_perimeter(contour):
+    length = len(contour)
+    perimeter = vector_norm(contour[0] - contour[length - 1])
+    for i in range(1, length):
+        length += vector_norm(contour[i] - contour[i - 1])
+    return perimeter
+
+
+def get_region_vector_angle(region, number_of_data):
     centroid = region.centroid()
     contours = region.contour()
 
     # table of the closest lower and higher deviated beams
-    results = [[None, None] for x in range(number_of_data)]
+    results = [[None, None]for x in range(number_of_data)]
 
     step = 2 * math.pi / number_of_data
 
     plt.figure()
     points_aux = []
-    points_aux.append(centroid)
+    points_aux.append([0, 0])
 
-    head_beam = centroid - contours[0]
+    head_beam = contours[0] - centroid
+    head_ang = deviation(head_beam)
+
     con_index = 0
-
     while con_index < len(contours):
-        beam = centroid - contours[con_index]
-        points_aux.append(contours[con_index])
-
-        beam_angle = deviation(head_beam, beam)
-        if abs(head_beam[0] - abs(centroid[0]) < abs(head_beam[0] - abs(centroid[0]))):
-            if abs(head_beam[1] - abs(centroid[1]) >= abs(head_beam[1] - abs(centroid[1]))):
-                beam_angle += math.pi
+        beam = contours[con_index] - centroid
+        points_aux.append(beam)
+        dev = deviation(beam)
+        beam_angle = dev - head_ang
+        if beam_angle < 0:
+            beam_angle += math.pi * 2
         distance = vector_norm(beam)
 
         closest_angle_pos = find_closest_angle_pos(beam_angle, number_of_data)
         closest_angle = closest_angle_pos * step
+        print ("Angle: " + str(math.degrees(closest_angle)))
+        print(distance)
         #TODO - precision
         #TODO - precision multiplier
-        pr = 0.9
         if (closest_angle - beam_angle) < 0.00001:
             for i in [0, 1]:
                 data_entry = results[closest_angle_pos][i]
-                if data_entry is None or data_entry[1] < distance * pr:
+                if data_entry is None or data_entry[1] < distance:
                     results[closest_angle_pos][i] = (beam_angle, distance)
         elif closest_angle < beam_angle:
             highest_lower = results[closest_angle_pos][0]
-            if highest_lower is None or highest_lower[1] < distance * pr or highest_lower[0] < beam_angle:
+            if highest_lower is None or highest_lower[1] < distance or highest_lower[0] < beam_angle:
                 results[closest_angle_pos][0] = (beam_angle, distance)
         else:
             lowest_higher = results[closest_angle_pos][1]
-            if lowest_higher is None or lowest_higher[1] < distance * pr or lowest_higher[0] > beam_angle:
+            if lowest_higher is None or lowest_higher[1] < distance or lowest_higher[0] > beam_angle:
                 results[closest_angle_pos][1] = (beam_angle, distance)
-
         con_index += 1
+
+    for i in range(number_of_data):
+        print (math.degrees(step * i))
+        print (results[i][0])
+        print (results[i][1])
+
+    for i in range(number_of_data):
+        if results[i][0] is None:
+            results[i][0] = (step * i, 0)
+        if results[i][1] is None:
+            results[i][1] = (step * i, 0)
 
     for i in range(number_of_data):
         print (step * i * (180 / math.pi))
         print (results[i][0])
         print (results[i][1])
-    results = [0 if h_l is None or l_h is None else interpolation(h_l[0], l_h[0], h_l[1], l_h[1]) for h_l, l_h in results]
+
+    results = [interpolation(h_l[0], l_h[0], h_l[1], l_h[1]) for h_l, l_h in results]
+    plt.scatter(range(100, number_of_data + 100), [results[x] for x in range(number_of_data)], c='r')
 
     plt.hold(True)
+    plt.axis('equal')
     xs = [x[1] for x in points_aux]
     ys = [x[0] for x in points_aux]
+    plt.scatter(xs, ys, c="c")
 
-    # res_x = [math.cos(x * step - deviation(head_beam, (0, 1))) * results[x] + centroid[0] for x in range(number_of_data)]
-    # res_y = [math.sin(x * step - deviation(head_beam, (0, 1))) * results[x] + centroid[0] for x in range(number_of_data)]
-    res_x = [range(number_of_data)]
-    res_y = [results[x] for x in range(number_of_data)]
+    res_aux = []
+    for i in range(number_of_data):
+        y = math.sin(step * i) * results[i]
+        x = math.cos(step * i) * results[i]
+        res_aux.append((x, y))
+
+    xs = [x[1] for x in res_aux]
+    ys = [x[0] for x in res_aux]
     plt.scatter(xs, ys)
-    plt.scatter(res_x, res_y)
+
     plt.show(True)
 
     return results
@@ -147,8 +215,10 @@ def dot_product(u, v):
     return sum((x * y) for x, y in zip(u, v))
 
 
-def deviation(u, v):
-    ang = math.acos(dot_product(u, v) / ((vector_norm(u)) * vector_norm(v)))
+def deviation(u):
+    ang = math.atan2(u[1], u[0])
+    if ang < 0:
+        ang += 2 * math.pi
     return ang
 
 
@@ -164,20 +234,5 @@ def interpolation(first_angle, sec_angle, first_dist, second_dist):
 
 
 if __name__ == '__main__':
-    print ((180/math.pi) *(deviation((1, 0), (1, 0))))
-    print ((180/math.pi) *(deviation((1, 0), (1, 1)))  )
-    print ((180/math.pi) *(deviation((1, 0), (0, 1)))   )
-    print ((180/math.pi) *(deviation((1, 0), (-1, 1)))   )
-    print ((180/math.pi) *(deviation((1, 0), (-1, 0)))    )
-    print ((180/math.pi) *(deviation((1, 0), (-1, -1)))    )
-    print ((180/math.pi) *(deviation((1, 0), (-1, 0)))      )
-    print ((180/math.pi) *(deviation((1, 0), (1, -1)))       )
-    # number_of_data = 9
-    # degrees = range(3610)
-    # for deg in degrees:
-    #     print(deg, find_closest_angle_pos((math.pi / 180) * (deg / 10), number_of_data))
-    # print(601, find_closest_angle_pos((math.pi / 180) * (601 / 10), number_of_data))
-
-
     chunks = project.gm.chunk_list()
     get_pca(chunks[:1], NUMBER_OF_DATA)
