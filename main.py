000ead929daa96e1ffcf8e1f9c2ccb27b8ed5be1
Filip Naiser <filip@naiser.cz>
2016-06-30 10:38:57 +0200

    ...

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ S_.general.print_log = False
 # This is development speed up process (kind of fast start). Runs only on developers machines...
 if is_flipajs_pc():
     sn_id = 2
-    name = 'Cam1'
+    name = 'big_lenses'
     wd = '/Users/flipajs/Documents/wd/GT/'
     # snapshot = {'chm': wd+name+'/.auto_save/'+str(sn_id)+'__chunk_manager.pkl',
     #             'gm': wd+name+'/.auto_save/'+str(sn_id)+'__graph_manager.pkl'}
 
-    project.load(wd+name+'/Cam1.fproj')
+    project.load(wd+name+'/big_lenses.fproj')
     ex.widget_control('load_project', project)
 
 
