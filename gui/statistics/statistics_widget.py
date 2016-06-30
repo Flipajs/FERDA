74a55e2d4ab6f1d2e67c756c1d5d45f94ec05911
Filip Naiser <filip@naiser.cz>
2016-06-30 17:45:34 +0200

    test2...

--- gui/statistics/statistics_widget.py
+++ gui/statistics/statistics_widget.py
@@ -186,7 +186,7 @@ class StatisticsWidget(QtGui.QWidget):
 
         t2 = time.time()
         file_num = 0
-        for ch in self.project.gm.chunk_list():
+        for _, ch in self.project.chm.chunks_.iteritems():
             rch = RegionChunk(self.project.chm[ch], self.project.gm, self.project.rm)
             d = self.init_struct_(rch[0])
 
@@ -199,6 +199,7 @@ class StatisticsWidget(QtGui.QWidget):
 
             if curr_size > limit:
                 with open(self.get_out_path()+str(file_num)+'.mat', 'wb') as f:
+                    print "saving ", str(file_num)
                     sio.savemat(f, {'FERDA': obj_arr})
 
                 curr_size = 0
