21ed906b9639749300d3e7341710b7696c564888
Simon Mandlik <simon.mandlik@gmail.com>
2016-08-09 11:54:30 +0200

    added line segment intersect module

--- scripts/frame_search/frame_search.py
+++ scripts/frame_search/frame_search.py
@@ -1,3 +1,4 @@
+from scipy.spatial import ConvexHull
 from core.project.project import Project
 from matplotlib import pyplot as plt
 
@@ -5,64 +6,82 @@ from matplotlib import pyplot as plt
 class FrameSearch:
 
     def __init__(self, project, frame):
+        """
+
+        :param project:
+        :param frame:
+        """
         self.project = project
         self.frame = frame
-        self.chunks = project.chm.chunks_in_frame(frame)
+        self.chunks_id = project.chm.chunks_in_frame(frame)
+        self.contours = self._prepare_contours(self.chunks_id)
+        self.hulls = self._prepare_convex_hulls(self.chunks_id)
 
-    def find_point(self, point=None, x=None, y=None, convex_hall=1):
+
+    def find_point(self, x, y):
         """
         Finds out, whether there's a chunk in the given point
-        :param point: two-dimensional iterable of x, y coordinates
         :param x: x cooradinate
         :param y: y coordinate
-        :param convex_hall: convex hall approximation is used,
-                if off may give more accurate results, but takes more time
         :return: id of chunk present on point, -1 if there is none
         """
         pass
 
-    def find_range(self, pointA=None, pointB=None, xA=None, yA=None, xB=None, yB=None, convex_hall=1):
+    def find_range(self, xA, yA, xB, yB):
         """
         Finds out whether there are any chunks present in the given range
-        :param pointA: two-dimensional iterable of upper left corner point coordinates
-        :param pointB: two-dimensional iterable of upper lower right point coordinates
         :param xA: x coordinate of upper left corner
         :param yA: y coordinate of upper left corner
         :param xB: x coordinate of lower right corner
         :param yB: y coordinate of lower right corner
-        :param convex_hall: convex_hall: convex hall approximation is used,
-                if off may give more accurate results, but takes more time
         :return: list od ids of chunks present, -1 if there is none
         """
 
-    def find_closest_chunk(self, point=None, x=None, y=None, convex_hall=1):
+    def find_closest_chunk(self, x, y):
         """
         Returns the closest chunk to the given point. If the point lies already on chunk, its id is returned.
-        :param point: two-dimensional iterable of x, y coordinates
         :param x: x cooradinate
         :param y: y coordinate
-        :param convex_hall: convex hall approximation is used,
-                if off may give more accurate results, but takes more time
         :return: id of the closest chunk, -1 if there are no chunks in the frame
         """
         pass
-    
+
     def find_count_in_range(self):
         pass
 
     def visualize_frame(self):
-        for ch in self.chunks:
-            contour = self._get_contour(ch)
-
-            plt.scatter(*zip(*contour))
+        for contour, hull in zip(self.contours.values(), self.hulls.values()):
+            plt.scatter(*zip(*contour), c='r')
+            plt.plot(*zip(*hull), c='b')
         plt.axis('equal')
         plt.show()
+
+    def _prepare_contours(self, chunks):
+        d = {}
+        for ch in chunks:
+            d[ch] = self._get_contour(ch)
+        return d
 
     def _get_contour(self, chunk):
         return project.gm.region(chunk[self.frame]).contour_without_holes()
 
-    def _prepare_frame(self):
-        pass
+    def _prepare_convex_hulls(self, chunks):
+        d = {}
+        for ch in chunks:
+            d[ch] = self._get_convex_hull(self._get_contour(ch))
+        return d
+
+    def _get_convex_hull(self, points):
+        hull = ConvexHull(points)
+        hull_indices = hull.vertices
+        return points[hull_indices, :]
+
+    def _point_in_area(self, x, y, points):
+        last = points[-1]
+        for point in points:
+            pass
+
+
 
 
 if __name__ == "__main__":
---
+++ scripts/frame_search/line_segment_intersect.py
@@ -0,0 +1,35 @@
+def on_segment(p1, p2, p3):
+    return max(p1[0], p3[0]) >= p2[0] >= min(p1[0], p3[0]) and \
+           max(p1[1], p3[1]) >= p2[1] >= min(p1[1], p3[1])
+
+
+def orientation(p1, p2, p3):
+    value = (p2[1] - p1[1]) * (p3[0] - p2[0]) - \
+              (p2[0] - p1[0]) * (p3[1] - p2[1])
+ 
+    if value == 0:
+        return 0
+ 
+    return 1 if value > 0 else 2
+
+
+def do_intersect(pointA, pointB, pointC, pointD):
+    o1 = orientation(pointA, pointB, pointC)
+    o2 = orientation(pointA, pointB, pointD)
+    o3 = orientation(pointC, pointD, pointA)
+    o4 = orientation(pointC, pointD, pointB)
+    
+    if o1 != o2 and o3 != o4:
+        return True
+    
+    return o1 == 0 and on_segment(pointA, pointC, pointB) or \
+            o2 == 0 and on_segment(pointA, pointD, pointB) or \
+            o3 == 0 and on_segment(pointC, pointA, pointD) or \
+            o4 == 0 and on_segment(pointC, pointB, pointD)
+
+if __name__ == "__main__":
+    points = [((1,1), (10,1), (1,2), (10,2)),
+              ((10,0), (0,10), (0,0), (10,10)),
+              ((-5, -5), (0,0), (1,1), (10,10))]
+    for pointA, pointB, pointC, pointD in points:
+        print do_intersect(pointA, pointB, pointC, pointD)
