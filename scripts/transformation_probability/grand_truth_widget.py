0ab8abbc291b15d72f45a55154ad5cf483f0b051
Simon Mandlik <simon.mandlik@gmail.com>
2016-08-11 09:56:59 +0200

    widget done

---
+++ scripts/transformation_probability/grand_truth_widget.py
@@ -0,0 +1,90 @@
+import sys
+from PyQt4 import QtGui, QtCore
+
+from gui.gui_utils import cvimg2qtpixmap
+
+
+class GrandTruthWidget(QtGui.QWidget):
+    def __init__(self, project):
+        super(GrandTruthWidget, self).__init__()
+        self.project = project
+
+        self.setLayout(QtGui.QVBoxLayout())
+        self.buttons_l = QtGui.QHBoxLayout()
+        self.imgs = QtGui.QHBoxLayout()
+        self.left = QtGui.QVBoxLayout()
+        self.left_img = QtGui.QLabel()
+        self.right = QtGui.QVBoxLayout()
+        self.right_img = QtGui.QLabel()
+
+
+        self.no = QtGui.QPushButton('no (N)', self)
+        self.yes = QtGui.QPushButton('yes (M)', self)
+        self.no_action = QtGui.QAction('no', self)
+        self.yes_action = QtGui.QAction('yes', self)
+
+        self._prepare_layouts()
+        self._prepare_buttons()
+
+        self.setWindowTitle('Grand Truth Widget')
+
+        self.results = {}
+        self.regions = None
+
+    def set_data(self, regions):
+        self.regions = regions
+        self._next()
+
+    def _next(self):
+        if self.regions:
+            self.current = self.regions.pop()
+            self._resolve(self.current[0], self.current[1])
+        else:
+            QtCore.QCoreApplication.quit()
+
+    def get_results(self):
+        return self.results
+
+    def _resolve(self, r1, r2):
+        self._add_region_left(r1)
+        self._add_region_right(r2)
+
+    def _add_region_left(self, r):
+        img = self.project.img_manager.get_crop(r.frame(), r, width=300, height=300)
+        self.left_img.setPixmap(cvimg2qtpixmap(img))
+
+    def _add_region_right(self, r):
+        img = self.project.img_manager.get_crop(r.frame(), r, width=300, height=300)
+        self.right_img.setPixmap(cvimg2qtpixmap(img))
+
+    def _prepare_layouts(self):
+        self.layout().addLayout(self.imgs)
+        self.imgs.addLayout(self.left)
+        self.imgs.addLayout(self.right)
+        self.layout().addLayout(self.buttons_l)
+
+        self.left.addWidget(self.left_img)
+        self.right.addWidget(self.right_img)
+        self.left.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
+        self.right.setAlignment(QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom)
+        self.buttons_l.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignTop)
+
+    def _prepare_buttons(self):
+        self.buttons_l.addWidget(self.no)
+        self.buttons_l.addWidget(self.yes)
+        self.no_action.triggered.connect(self._no_action)
+        self.yes_action.triggered.connect(self._yes_action)
+        self.no_action.setShortcut(QtGui.QKeySequence(QtCore.Qt.Key_N))
+        self.yes_action.setShortcut(QtGui.QKeySequence(QtCore.Qt.Key_M))
+        self.no.addAction(self.no_action)
+        self.yes.addAction(self.yes_action)
+        self.yes.setFixedWidth(100)
+        self.no.setFixedWidth(100)
+
+    def _no_action(self):
+        self.results[self.current] = False
+        self._next()
+
+    def _yes_action(self):
+        self.results[self.current] = True
+        self._next()
--- scripts/transformation_probability/transformation_classifier.py
+++ scripts/transformation_probability/transformation_classifier.py
@@ -1,7 +1,17 @@
+from PyQt4 import QtGui
+
 import numpy as np
+import sys
 
 from core.project.project import Project
+from scripts.transformation_probability.grand_truth_widget import GrandTruthWidget
 
+results = None
+widget = None
+
+def get_results():
+    results = widget.get_results()
+    print results
 
 def descriptor(r1, r2):
     # centroid distance
@@ -20,11 +30,20 @@ def descriptor(r1, r2):
     # axis difference
     axis_diff = r2.major_axis_ - r1.major_axis_
 
-    return (centr_dist, margin_diff, max_intensity, min_intensity, area_diff, axis_diff)
-
+    return centr_dist, margin_diff, max_intensity, min_intensity, area_diff, axis_diff
 
 if __name__ == "__main__":
     project = Project()
     project.load("/home/simon/FERDA/projects/Cam1_/cam1.fproj")
 
     print descriptor(project.rm[1], project.rm[2])
+
+    app = QtGui.QApplication(sys.argv)
+
+    widget = GrandTruthWidget(project)
+    widget.set_data([(project.rm[1], project.rm[2]), (project.rm[2], project.rm[3])])
+    widget.show()
+    app.aboutToQuit.connect(get_results)
+    sys.exit(app.exec_())
+
+
