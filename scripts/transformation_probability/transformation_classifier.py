360d7ca07da0635bed0ed4b2e66af92a50df39b3
Simon Mandlik <simon.mandlik@gmail.com>
2016-08-17 15:25:25 +0200

    ...

--- scripts/transformation_probability/transformation_classifier.py
+++ scripts/transformation_probability/transformation_classifier.py
@@ -1,10 +1,14 @@
 import logging
 import random
 import sys
+from IN import minor
 from PyQt4 import QtGui
 import os
 import numpy as np
+from numpy.linalg import norm
 from os.path import exists
+import cPickle as pickle
+from scipy.spatial.qhull import ConvexHull
 from sklearn.ensemble import RandomForestClassifier
 from transformation_trainer import TransformationTrainer, hash_region_tuple
 import view_widget
@@ -28,7 +32,7 @@ class TransformationClassifier():
         self.fname = os.path.join(self.project.working_directory, FNAME)
         logging.info("Loading previous descriptors from %s" % self.fname)
         if exists(self.fname):
-            self.feature_vectors = np.pickle.load(open(self.fname, 'rb'))
+            self.feature_vectors = pickle.load(open(self.fname, 'rb'))
         else:
             self.feature_vectors = {}
         logging.info("Loaded {0} descriptors from database".format(len(self.results)))
@@ -58,7 +62,7 @@ class TransformationClassifier():
 
     def test(self, regions):
         accuracies = []
-        for seed in range(0):
+        for seed in range(10):
             random.seed(seed)
             np.random.seed(seed)
             random.shuffle(regions)
@@ -72,22 +76,26 @@ class TransformationClassifier():
         logging.info("Median: {0:.3f}".format(np.median(accuracies)))
         logging.info("Std: {0:.3f}".format(np.std(accuracies)))
 
-        # random.shuffle(regions)
-        # training_regions = regions[len(regions) / 2:]
-        # testing_regions = regions[:len(regions) / 2]
-        # t = filter(lambda x: results[hash_region_tuple(x)], testing_regions)
-        # accuracy = self.compute_accuracy(training_regions, t, save_results=True)
-        # logging.info("True classified with {0:.3f} accuracy".format(accuracy))
-        # f = filter(lambda x: not results[hash_region_tuple(x)], testing_regions)
-        # accuracy = self.compute_accuracy(training_regions, f, save_results=True)
-        # logging.info("False classified with {0:.3f} accuracy".format(accuracy))
+        random.shuffle(regions)
+        training_regions = regions[len(regions) / 2:]
+        testing_regions = regions[:len(regions) / 2]
+        t = filter(lambda x: results[hash_region_tuple(x)], testing_regions)
+        accuracy = self.compute_accuracy(training_regions, t, save_results=True)
+        logging.info("True classified with {0:.3f} accuracy".format(accuracy))
+        f = filter(lambda x: not results[hash_region_tuple(x)], testing_regions)
+        accuracy = self.compute_accuracy(training_regions, f, save_results=True)
+        logging.info("False classified with {0:.3f} accuracy".format(accuracy))
 
     def view_results(self):
         regions = [k for k, v in self.classification.items() if
                    (bool(v) != self.results[hash_region_tuple(k)])]
-        avg_vector = np.mean(np.array(self.feature_vectors.values(), axis=0))
-        # widget = view_widget.ViewWidget(self.project, regions, self.classification, self.probability, self, avg_vector)
-        # widget.show()
+        n_correct = len(regions)
+        regions += [k for k, v in self.classification.items() if
+                    (bool(v) == self.results[hash_region_tuple(k)])]
+        avg_vector = (np.array(self.feature_vectors.values()).mean(axis=0))
+        widget = view_widget.ViewWidget(self.project, regions, self.classification, self.probability, self, avg_vector,
+                                        n_correct)
+        widget.show()
         app.exec_()
 
     def feature_vector(self, regions):
@@ -96,9 +104,9 @@ class TransformationClassifier():
         ret = []
 
         # centroid distance
-        centr_dist = np.linalg.norm(r2.centroid() - r1.centroid())
-        ret.append(centr_dist
-                   )
+        centr_dist = norm(r2.centroid() - r1.centroid())
+        ret.append(centr_dist)
+
         # margin difference
         margin_diff = abs(r2.margin() - r1.margin())
         ret.append(margin_diff)
@@ -114,39 +122,107 @@ class TransformationClassifier():
         ret.append(area_diff)
 
         # axis difference
-        axis_diff = abs(r2.major_axis_ - r1.major_axis_)
-        ret.append(axis_diff)
+        major_axis_diff = abs(r2.major_axis_ - r1.major_axis_)
+        minor_axis_diff = abs(r2.minor_axis_ - r1.minor_axis_)
+        ret.append(major_axis_diff)
+        ret.append(minor_axis_diff)
 
-        regions_t1 = map(lambda x: self.project.gm.region(x).centroid(), self.project.gm.get_vertices_in_t(r1.frame()))
-        regions_t2 = map(lambda x: self.project.gm.region(x).centroid(), self.project.gm.get_vertices_in_t(r2.frame()))
+        regions_t1 = map(lambda x: self.project.gm.region(x), self.project.gm.get_vertices_in_t(r1.frame()))
+        regions_t2 = map(lambda x: self.project.gm.region(x), self.project.gm.get_vertices_in_t(r2.frame()))
+        regions_t1 = filter(lambda x: x != r1, regions_t1)
+        regions_t2 = filter(lambda x: x != r2, regions_t2)
         m = self.project.stats.major_axis_median
+
+        # small centroid neighbourhood
+        ret.append(abs(self.count_regions_in_neighbourhood_contour(r1, regions_t1, R1 * m) -
+                       self.count_regions_in_neighbourhood_contour(r2, regions_t2, R1 * m)))
+        # bigger centroid neighbourhood
+        ret.append(abs(self.count_regions_in_neighbourhood_contour(r1, regions_t1, R2 * m) -
+                       self.count_regions_in_neighbourhood_contour(r2, regions_t2, R2 * m)))
+        # the centroid biggest neighbourhood
+        ret.append(abs(self.count_regions_in_neighbourhood_contour(r1, regions_t1, R3 * m) -
+                       self.count_regions_in_neighbourhood_contour(r2, regions_t2, R3 * m)))
+
         # small neighbourhood
-        ret.append(abs(self._count_regions_in_neighbourhood(r1, regions_t1, R1 * m) -
-                       self._count_regions_in_neighbourhood(r2, regions_t2, R1 * m)))
+        ret.append(abs(self.count_regions_in_neighbourhood(r1, regions_t1, R1 * m) -
+                       self.count_regions_in_neighbourhood(r2, regions_t2, R1 * m)))
         # bigger neighbourhood
-        ret.append(abs(self._count_regions_in_neighbourhood(r1, regions_t1, R2 * m) -
-                       self._count_regions_in_neighbourhood(r2, regions_t2, R2 * m)))
+        ret.append(abs(self.count_regions_in_neighbourhood(r1, regions_t1, R2 * m) -
+                       self.count_regions_in_neighbourhood(r2, regions_t2, R2 * m)))
         # the biggest neighbourhood
-        ret.append(abs(self._count_regions_in_neighbourhood(r1, regions_t1, R3 * m) -
-                       self._count_regions_in_neighbourhood(r2, regions_t2, R3 * m)))
+        ret.append(abs(self.count_regions_in_neighbourhood(r1, regions_t1, R3 * m) -
+                       self.count_regions_in_neighbourhood(r2, regions_t2, R3 * m)))
+
+        # closest and farthest distance between two regions
+        a = self.find_extreme_points(r1.contour_without_holes(), r2.contour_without_holes())
+        ret.append(a[0])
+        ret.append(a[1])
+
+        # closest and farthest distance among all
+        a = self.extremes_among_neighbours(r1, regions_t1)
+        b = self.extremes_among_neighbours(r2, regions_t2)
+        ret.append(abs(a[0] - b[0]))
+        ret.append(abs(a[1] - b[1]))
 
         return ret
 
     def descriptor_representation(self, regions):
         desc = self.feature_vector(regions)
-        features = ['centroid distance', 'margin difference', 'max_intensity', 'min_intensity', 'area difference',
-                    'axis difference', 'small_neigh', 'bigger_neigh', 'the biggest_neigh']
+        features = ['centroid dist', 'margin diff', 'max_intensity', 'min_intensity', 'area diff',
+                    'major_axis diff', 'minor_axis_diff', 'c_small_neigh', 'c_middle_neigh', 'c_big_neigh',
+                    'small_neigh', 'middle_neigh', 'big_neigh', 'dist_small', 'dist_big', 'dist_small_all',
+                    'dist_big_all']
         return zip(features, desc)
 
-    def _count_regions_in_neighbourhood(self, r, centroids, radius):
+    def count_regions_in_neighbourhood_contour(self, r, regions, radius):
         c = r.centroid()
-        return len(filter(lambda x: np.linalg.norm(c - x) < radius, centroids))
+        return len(filter(lambda x: norm(c - x.centroid()) < radius, regions))
+
+    def count_regions_in_neighbourhood(self, r, regions, radius):
+        c = r.centroid()
+        return len(filter(lambda x: self.find_closest_point(c, x.contour_without_holes()) < radius, regions))
+
+    def extremes_among_neighbours(self, r, regions):
+        c = r.contour_without_holes()
+        farthest = 0
+        closest = np.inf
+        for reg in regions:
+            dist = self.find_extreme_points(c, reg.contour_without_holes())
+            farthest = max(farthest, dist[0])
+            closest = min(closest, dist[1])
+        return farthest, closest
+
+    def find_closest_point(self, c, points):
+        convex_hull = ConvexHull(points)
+        convex_hull = points[convex_hull.vertices, :]
+        dist = norm(c - convex_hull[0])
+        for p in convex_hull[1:]:
+            d = norm(c - p)
+            dist = min(dist, d)
+        return dist
+
+    def find_extreme_points(self, points1, points2):
+        a = ConvexHull(points1)
+        a = points1[a.vertices, :]
+        b = ConvexHull(points2)
+        b = points2[b.vertices, :]
+        farthest = 0
+        closest = np.inf
+        for p in a:
+            for q in b:
+                dist = norm(p - q)
+                farthest = max(dist, farthest)
+                closest = min(dist, closest)
+        return farthest, closest
 
     def update_feature_vectors(self, regions, all=False):
-        regions = filter(lambda x: x not in self.feature_vectors, regions)
+        if not all:
+            regions = filter(lambda x: x not in self.feature_vectors, regions)
+        print len(regions)
         self.feature_vectors.update({r: self.feature_vector(r) for r in regions})
-        logging.info("Saving {0} descriptors to database. It now contains {1} entries.".format(len(regions),
-                                                                                               len(self.feature_vectors)))
+        logging.info("Saving {0} descriptors to database. It now contains {1} entries.".format(
+            len(regions), len(self.feature_vectors)))
+        pickle.dump(self.feature_vectors, open(self.fname, 'wb'))
 
 if __name__ == "__main__":
     project = Project()
--- scripts/transformation_probability/view_widget.py
+++ scripts/transformation_probability/view_widget.py
@@ -9,9 +9,10 @@ MARGIN = 300
 HEIGHT = 500
 WIDTH = 700
 
+MAX_ROW = 5
 
 class ViewWidget(QtGui.QWidget):
-    def __init__(self, project, regions, classification, probability, classifier, avg_feat_v):
+    def __init__(self, project, regions, classification, probability, classifier, avg_feat_v, n_correct):
         super(ViewWidget, self).__init__()
         self.project = project
         self.regions = regions
@@ -19,7 +20,7 @@ class ViewWidget(QtGui.QWidget):
         self.classifier = classifier
         self.probability = probability
         self.avg_feat_v = avg_feat_v
-        print avg_feat_v
+        self.n_correct = n_correct
 
         self.setLayout(QtGui.QVBoxLayout())
         self.buttons = QtGui.QHBoxLayout()
@@ -52,12 +53,21 @@ class ViewWidget(QtGui.QWidget):
         self.prev_b.setDisabled(True)
 
     def _prepare_table(self):
-        self.table.setColumnCount(2)
+        self.table.setColumnCount(3)
+        self.table.setRowCount(len(self.avg_feat_v))
         self.table.verticalHeader().setVisible(False)
-        self.table.horizontalHeader().setVisible(False)
         self.table.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
         self.table.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.table.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        for i, v in enumerate(self.avg_feat_v):
+            if i % MAX_ROW == 0:
+                self.table.setHorizontalHeaderItem(0, QTableWidgetItem(""))
+                self.table.setHorizontalHeaderItem(1, QTableWidgetItem(""))
+                self.table.setHorizontalHeaderItem(2, QTableWidgetItem("avg"))
+                self.table.horizontalHeaderItem(i + 2).setTextAlignment(QtCore.Qt.AlignRight)
+            self.table.setItem(i, 2, QTableWidgetItem(str("{0:.2f}".format(v))))
+            self.table.item(i, 2).setTextAlignment(QtCore.Qt.AlignRight)
+        self.table.resizeRowsToContents()
 
     def _view(self, r):
         self._add_region_left(r[0])
@@ -67,20 +77,20 @@ class ViewWidget(QtGui.QWidget):
         self.desc_label.setText("Feature Vector")
         self._set_table(self.classifier.descriptor_representation(r))
         self.info.setText(
-            "Tagged <b>{0}</b>, should be <b>{1}</b> with probability: F : {2} T : {3}".format(res, not res, p[0],
-                                                                                               p[1]))
+            "Tagged <b>{0}</b>, should be <b>{1}</b> with probability: F : {2} T : {3}".format(res,
+                                        not res if self.current_index < self.n_correct else res, p[0], p[1]))
 
     def _set_table(self, data):
-        self.table.setRowCount(len(data))
         for i, (f, v) in enumerate(data):
-            self.table.setItem(i, 0, QTableWidgetItem(f))
-            self.table.item(i, 0).setTextAlignment(QtCore.Qt.AlignRight)
-            self.table.setItem(i, 1, QTableWidgetItem("{0:.2f}".format(v)))
-            self.table.item(i, 1).setTextAlignment(QtCore.Qt.AlignRight)
-
+            a = i % MAX_ROW
+            ratio = i / MAX_ROW
+            self.table.setItem(a, ratio, QTableWidgetItem(f))
+            self.table.item(a, ratio).setTextAlignment(QtCore.Qt.AlignRight)
+            self.table.setItem(a, ratio + 1, QTableWidgetItem("{0:.2f}".format(v)))
+            self.table.item(a, ratio + 1).setTextAlignment(QtCore.Qt.AlignRight)
         self.table.resizeColumnsToContents()
-        self.table.setFixedSize(self.table.horizontalHeader().length() + self.table.verticalHeader().width(),
-                                self.table.verticalHeader().length())
+        self.table.setFixedSize(self.table.horizontalHeader().length(),
+                                self.table.verticalHeader().length() + self.table.horizontalHeader().height())
 
     def _add_region_left(self, r):
         img = self.project.img_manager.get_crop(r.frame(), r, width=WIDTH, height=HEIGHT, margin=MARGIN)
