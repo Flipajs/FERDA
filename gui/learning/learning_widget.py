c71cfe03f8b2e25993c147d707608770d0a701de
Filip Naiser <filip@naiser.cz>
2016-08-24 14:33:47 +0200

    added tracklets info into learning_widget

--- gui/learning/learning_widget.py
+++ gui/learning/learning_widget.py
@@ -13,8 +13,10 @@ class LearningWidget(QtGui.QWidget):
         super(LearningWidget, self).__init__()
 
         self.project = project
+        self.hbox = QtGui.QHBoxLayout()
         self.vbox = QtGui.QVBoxLayout()
-        self.setLayout(self.vbox)
+        self.hbox.addLayout(self.vbox)
+        self.setLayout(self.hbox)
 
         self.lp = None
         if not self.project:
@@ -27,13 +29,15 @@ class LearningWidget(QtGui.QWidget):
 
         self.start_button = QtGui.QPushButton('start')
         self.start_button.clicked.connect(self.lp.run_learning)
+        self.vbox.addWidget(self.start_button)
 
         self.info_table = QtGui.QTableWidget()
         self.info_table.setColumnCount(2)
         self.info_table.setRowCount(10)
 
         self.info_table.setMinimumHeight(500)
-        self.vbox.addWidget(self.info_table)
+        self.info_table.setFixedWidth(220)
+        self.hbox.addWidget(self.info_table)
 
         # TODO: next step
         # TODO: next N steps
@@ -44,12 +48,14 @@ class LearningWidget(QtGui.QWidget):
 
         # TODO: update callback... info about decisions...
 
-        self.vbox.addWidget(self.start_button)
-
         self.tracklets_table = QtGui.QTableWidget()
-        self.tracklets_table.setRowCount(self.lp.undecided_tracklets)
+        self.tracklets_table.setRowCount(len(self.lp.undecided_tracklets))
         num_animals = len(self.project.animals)
-        self.tracklets_table.setColumnCount(2 * num_animals + 2)
+        self.tracklets_table.setColumnCount(2 * num_animals + 5)
+        self.tracklets_table.setMinimumWidth(1000)
+        self.tracklets_table.setMinimumHeight(1000)
+        self.tracklets_table.setSortingEnabled(True)
+        self.hbox.addWidget(self.tracklets_table)
 
         self.update_callback()
 
@@ -64,16 +70,51 @@ class LearningWidget(QtGui.QWidget):
         self.info_table.setItem(2, 1, QtGui.QTableWidgetItem(str(len(self.lp.undecided_tracklets))))
 
         self.info_table.setItem(3, 0, QtGui.QTableWidgetItem('#new T examples'))
-        self.info_table.setItem(3, 0, QtGui.QTableWidgetItem(str(len(self.lp.X) - self.lp.old_x_size)))
+        self.info_table.setItem(3, 1, QtGui.QTableWidgetItem(str(len(self.lp.X) - self.lp.old_x_size)))
 
         START = 4
         for i in range(len(self.project.animals)):
             self.info_table.setItem(START+i, 0, QtGui.QTableWidgetItem('#examples, ID: '+str(i)))
             self.info_table.setItem(START+i, 1, QtGui.QTableWidgetItem(str(np.count_nonzero(self.lp.y == i))))
 
+        # update tracklet info...
+        num_animals = len(self.project.animals)
+        self.tracklets_table.clear()
+        header_labels = ("id", "len", "start", "end", "cert")
+        for i in range(num_animals):
+            header_labels += ('m'+str(i), )
 
-        # update chunk infos...
+        for i in range(num_animals):
+            header_labels += (str(i), )
 
+        print "TYPE:", type(header_labels)
+        self.tracklets_table.setHorizontalHeaderLabels(header_labels)
+        for i, t_id in enumerate(self.lp.undecided_tracklets):
+            t = self.project.chm[t_id]
+
+            self.tracklets_table.setItem(i, 0, QtGui.QTableWidgetItem(str(t.id())))
+            self.tracklets_table.setItem(i, 1, QtGui.QTableWidgetItem(str(t.length())))
+            self.tracklets_table.setItem(i, 2, QtGui.QTableWidgetItem(str(t.start_frame(self.project.gm))))
+            self.tracklets_table.setItem(i, 3, QtGui.QTableWidgetItem(str(t.end_frame(self.project.gm))))
+            self.tracklets_table.setItem(i, 4, QtGui.QTableWidgetItem(self.__f2str(self.lp.tracklet_certainty[t_id])))
+
+            d = self.lp.tracklet_measurements[t_id]
+            for j in range(num_animals):
+                self.tracklets_table.setItem(i, 5+j, QtGui.QTableWidgetItem(self.__f2str(d[j])))
+
+            for j in range(num_animals):
+                val = ''
+                if j in self.lp.ids_not_present_in_tracklet[t_id]:
+                    val = 'N'
+                elif j in self.lp.ids_present_in_tracklet[t_id]:
+                    val = 'P'
+
+                self.tracklets_table.setItem(i, 5+num_animals+j, QtGui.QTableWidgetItem(val))
+
+        self.tracklets_table.resizeColumnsToContents()
+
+    def __f2str(self, f, prec=3):
+        return ('{:.'+str(prec)+'}').format(f)
 
     def load_project(self, default=''):
         path = ''
