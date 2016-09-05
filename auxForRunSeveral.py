6a76e8281f60e419001dbae6b623d62af9e15afe
Barbara Casillas Perez <casillas@debian>
2016-09-05 18:27:01 +0200

    added files for cluster usage

---
+++ auxForRunSeveral.py
@@ -0,0 +1,35 @@
+import sys
+import subprocess
+import os
+import datetime
+import pipes
+
+#from http://stackoverflow.com/a/14392472/3074835  by Dietrich Epp
+def exists_remote(host, path):
+	proc = subprocess.Popen(
+		['ssh', host, 'test -e %s' % pipes.quote(path)])
+	proc.wait()
+	return proc.returncode == 0
+    
+def exists_local_directory(path):
+	if not os.path.exists(path):
+	   os.mkdir(path)
+	
+def longDate():
+	Y = format(datetime.datetime.now().year,'04')
+	M = format(datetime.datetime.now().month,'02')
+	D = format(datetime.datetime.now().day,'02')
+	h = format(datetime.datetime.now().hour,'02')
+	m = format(datetime.datetime.now().minute,'02')
+	s = format(datetime.datetime.now().second,'02')
+	
+	return Y+M+D+"_"+h+m+s
+  
+
+def fixPath(path):
+	if (path[len(path)-1] == "/"):
+		ret = path;
+	else:
+		ret = path+"/";
+	
+	return ret;
