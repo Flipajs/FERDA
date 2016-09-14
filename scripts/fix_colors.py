b1391a33e0d84068fc0a0464a472027db37e8b54
Filip Naiser <filip@naiser.cz>
2016-09-14 13:58:15 +0200

    added fix_color script

---
+++ scripts/fix_colors.py
@@ -0,0 +1,25 @@
+import sys
+import cPickle as pickle
+import random
+from PyQt4 import QtGui
+
+"""
+Use this script in case when color manager was disabled.
+Goes through all chunks and set random color.
+"""
+
+
+if __name__ == "__main__":
+    working_dir = sys.argv[1]
+
+    with open(working_dir+'/chm.pkl', 'rb') as f:
+        chm = pickle.load(f)
+
+    for ch in chm.chunk_gen():
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        ch.color = QtGui.QColor.fromRgb(r, g, b)
+
+    with open(working_dir+'/chm.pkl', 'wb') as f:
+        pickle.dump(chm, f)
