1f76a20de744cf5e1248fc3d038ce75b3bf12b1d
Filip Naiser <filip@naiser.cz>
2016-09-21 16:09:41 +0200

    bg_computer_assemgling duplicate chunks fix

--- core/bg_computer_assembling.py
+++ core/bg_computer_assembling.py
@@ -153,6 +153,9 @@ def merge_parts(new_gm, old_g, old_g_relevant_vertices, project, old_rm, old_chm
     used_chunks_ids = set()
     # reindex vertices
     for v_id in old_g_relevant_vertices:
+        if not old_g.vp['active'][v_id]:
+            continue
+
         old_v = old_g.vertex(v_id)
         old_reg = old_rm[old_g.vp['region_id'][old_v]]
         new_rm.add(old_reg)
