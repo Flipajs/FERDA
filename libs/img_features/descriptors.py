8ccf9638d3b7c39c0922991c9874c1d5a1d5efca
Filip Naiser <filip@naiser.cz>
2017-04-18 13:17:10 +0200

    added color_naming and imd_features packages

---
+++ libs/img_features/descriptors.py
@@ -0,0 +1,39 @@
+import numpy as np
+import color_naming
+from utils import feature_descriptor
+
+
+def colornames_descriptor(im, block_division=(2, 2), pyramid_levels=3, histogram_density=False):
+    """
+
+    Parameters
+    ----------
+    im : np.array, image with 3 channels in RGB order (be aware that cv2.imread() returns BGR ...)
+
+    for further details see utils.py - feature_descriptor parameters
+    block_division : tuple(uint, uint)
+    pyramid_levels : uint
+    histogram_density : bool
+
+    Returns
+    -------
+
+    """
+
+    # get color naming
+    cm = color_naming.im2colors(im)
+
+    # it is given by 11 color names
+    histogram_num_bins = 11
+
+    return feature_descriptor(cm, block_division=block_division, pyramid_levels=pyramid_levels,
+                              histogram_num_bins=histogram_num_bins, histogram_density=histogram_density)
+
+
+if __name__ == '__main__':
+    from scipy.misc import imread
+
+    im = imread('data/car.jpg')
+
+    f = colornames_descriptor(im, histogram_density=True)
+    print f
--- main.py
+++ main.py
@@ -22,18 +22,32 @@ project = Project()
 S_.general.print_log = False
 
 
-def test1():
-    for n in project.gm.g.vertices():
-        pass
+# def test1():
+#     for n in project.gm.g.vertices():
+#         pass
+#
+#
+# def test2():
+#     for i, n in enumerate(project.gm.g.vertices()):
+#         pass
+#
+#
+# def test3():
+#     a = project.gm.g.get_vertices()
+#     for i, n in enumerate(np.nditer(project.gm.g.get_vertices())):
+#         pass
 
+def test1():
+    for n in project.gm.g.edges():
+        pass
 
 def test2():
-    for i, n in enumerate(project.gm.g.vertices()):
+    for i, n in enumerate(project.gm.g.edges()):
         pass
 
-
 def test3():
-    for i, n in np.nditer(project.gm.g.get_vertices()):
+    a = project.gm.g.get_vertices()
+    for i, n in enumerate(np.nditer(project.gm.g.get_edges())):
         pass
 
 # This is development speed up process (kind of fast start). Runs only on developers machines...
