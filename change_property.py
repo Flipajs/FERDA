587455f00388022c49bbaa9db8b1750b02173fde
Barbara Casillas Perez <casillas@debian>
2017-04-25 20:23:26 +0200

    cluster usage (eg hold_jid)

--- change_property.py
+++ change_property.py
@@ -5,8 +5,12 @@ def change_pr(path,value,property):
     pathToProj=path
     with open(pathToProj,'rb') as f:
         d=pickle.load(f)
+    if property=='min_area':
+        d['mser_parameters'].min_area=value
     if property=='video_paths':
         d[property]=[value]
+    if property=='optimisation':
+        d['other_parameters'].segmentation_use_roi_prediction_optimisation=value
     else:
         d[property]=value
     pickle.dump(d,open(pathToProj,'wb'))
