c10ee1255f289467969035527e0c9e8f13bdf99d
Filip Naiser <filip@naiser.cz>
2017-11-17 13:34:45 +0100

    tuining cnn architecture

--- scripts/CNN/train.py
+++ scripts/CNN/train.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
     # x = MaxPooling2D((2, 2))(x)
 
     # x = Conv2D(64, (3, 3))(x)
-    x = Conv2D(64, (3, 3), dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
     x = Conv2D(32, (3, 3))(x)
     x = MaxPooling2D((2, 2))(x)
     x = Conv2D(16, (3, 3))(x)
