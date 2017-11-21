d0871919d4fbf4f61346565e566f8e2dbfe09f20
Filip Naiser <filip@naiser.cz>
2017-11-21 15:51:27 +0100

    ..

--- core/graph/chunk_manager.py
+++ core/graph/chunk_manager.py
@@ -3,6 +3,7 @@ __author__ = 'flipajs'
 from chunk import Chunk
 from libs.intervaltree.intervaltree import IntervalTree
 from utils.misc import print_progress
+from tqdm import tqdm
 
 
 class ChunkManager:
@@ -120,14 +121,8 @@ class ChunkManager:
         self.itree = IntervalTree()
 
         chn = len(self)
-        if chn:
-            for i, ch in enumerate(self.chunk_gen()):
-                self._add_ch_itree(ch, gm)
-
-                if i % 100:
-                    print_progress(i, chn, "reseting chunk interval tree")
-
-            print_progress(i, chn, "reseting chunk interval tree", "DONE\n")
+        for i, ch in tqdm(enumerate(self.chunk_gen()), total=chn):
+            self._add_ch_itree(ch, gm)
 
     def add_single_vertices_chunks(self, p, frames=None):
         self.reset_itree(p.gm)
