67f15280c606e6da87d547397c4378e551b7e6b2
Filip Naiser <filip@naiser.cz>
2017-11-22 21:25:14 +0100

    shortcut CNN

--- scripts/CNN/train_interactions.py
+++ scripts/CNN/train_interactions.py
@@ -105,8 +105,6 @@ def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2))
     return x
 
 
-
-
 def model():
     global NUM_PARAMS, DATA_DIR, CONTINUE
 
@@ -117,33 +115,22 @@ def model():
     else:
         bn_axis = 1
 
-    x = Conv2D(
-        64, (7, 7), strides=(2, 2), padding='same', name='conv1')(img_input)
-    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    shortcut = Conv2D(64, (5, 5), strides=(2, 2), padding='same', name='conv1')(img_input)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(shortcut)
     x = Activation('relu')(x)
     x = MaxPooling2D((3, 3), strides=(2, 2))(x)
-
-    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
-    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
-    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
-
-    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
-    x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
-
-    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
-    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
-
-    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
-    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
-    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
-
+    x = Conv2D(32, (3, 3))(x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    x = MaxPooling2D((3, 3))(x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3))(x)
+    x = Conv2D(32, (9, 9))(x)
     x = AveragePooling2D((7, 7), name='avg_pool')(x)
+
+    x = layers.concatenate([x, shortcut])
+    x = Activation('relu')(x)
+    x = Dense(256)(x)
 
     x = Flatten()(x)
     out = Dense(NUM_PARAMS, activation='linear', name='fc1000')(x)
@@ -159,6 +146,59 @@ def model():
                   optimizer='adam')
 
     return model
+
+# def model():
+#     global NUM_PARAMS, DATA_DIR, CONTINUE
+#
+#     img_input = Input(shape=(200, 200, 3))
+#
+#     if K.image_data_format() == 'channels_last':
+#         bn_axis = 3
+#     else:
+#         bn_axis = 1
+#
+#     x = Conv2D(
+#         64, (7, 7), strides=(2, 2), padding='same', name='conv1')(img_input)
+#     x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+#     x = Activation('relu')(x)
+#     x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+#
+#     x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
+#     x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
+#     x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
+#
+#     x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
+#     x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
+#     x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
+#     x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
+#
+#     # x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
+#     # x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
+#     # x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
+#     # x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
+#     # x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
+#     # x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
+#     #
+#     # x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+#     # x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+#     # x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+#
+#     x = AveragePooling2D((7, 7), name='avg_pool')(x)
+#
+#     x = Flatten()(x)
+#     out = Dense(NUM_PARAMS, activation='linear', name='fc1000')(x)
+#
+#     model = Model(img_input, out, name='resnet50')
+#     # model = Model(input_shape, out)
+#
+#     if CONTINUE:
+#         model.load_weights(DATA_DIR + "/interaction_weights_"+str(NUM_PARAMS)+".h5")
+#
+#     model.summary()
+#     model.compile(loss='mean_squared_error',
+#                   optimizer='adam')
+#
+#     return model
 
 
 
