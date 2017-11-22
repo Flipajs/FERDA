c9524afdfcab3abaa1cfbba5e149819968aa84ea
Filip Naiser <filip@naiser.cz>
2017-11-22 22:31:14 +0100

    cleanup

--- scripts/CNN/train_interactions.py
+++ scripts/CNN/train_interactions.py
@@ -33,13 +33,9 @@ def model():
 
     img_input = Input(shape=(200, 200, 3))
 
-    if K.image_data_format() == 'channels_last':
-        bn_axis = 3
-    else:
-        bn_axis = 1
+    # build the VGG16 network with our input_img as input
 
     x = Conv2D(64, (3, 3), padding='same', activation='relu')(img_input)
-    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
     x = Conv2D(64, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(64, (3, 3), padding='same', activation='relu')(x)
     x = MaxPooling2D((2, 2))(x)
@@ -49,18 +45,14 @@ def model():
     x = Conv2D(64, (3, 3), padding='same', activation='relu')(x)
     x = Conv2D(64, (3, 3), padding='same', activation='relu')(x)
     x = MaxPooling2D((2, 2))(x)
-    x = Conv2D(32, (3, 3), dilation_rate=(2, 2), padding='same', activation='relu')(x)
-    x = Conv2D(32, (3, 3), dilation_rate=(4, 4), padding='same', activation='relu')(x)
-    x = MaxPooling2D((2, 2))(x)
-    x = Conv2D(32, (3, 3), dilation_rate=(8, 8), padding='same', activation='relu')(x)
-    x = Conv2D(32, (3, 3), dilation_rate=(8, 8), padding='same', activation='relu')(x)
-    x = MaxPooling2D((2, 2))(x)
-    x = Conv2D(32, (3, 3), dilation_rate=(16, 16), padding='same', activation='relu')(x)
-    x = Conv2D(32, (3, 3), dilation_rate=(16, 16), padding='same', activation='relu')(x)
-    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(64, (3, 3), dilation_rate=(2, 2), activation='relu')(x)
+    x = ZeroPadding2D((1, 1))(x)
+    x = Conv2D(64, (3, 3), dilation_rate=(4, 4), activation='relu')(x)
+    x = ZeroPadding2D((1, 1))(x)
+    x = Conv2D(64, (3, 3), dilation_rate=(8, 8), activation='relu')(x)
+    x = ZeroPadding2D((1, 1))(x)
 
     x = Flatten()(x)
-    # x = Dense(64, activation='relu')(x)
     x = Dense(NUM_PARAMS, activation='linear')(x)
     model = Model(img_input, x)
 
@@ -69,6 +61,117 @@ def model():
                   optimizer='adam')
 
     return model
+
+# def model():
+#     global NUM_PARAMS, DATA_DIR, CONTINUE
+#
+#     img_input = Input(shape=(200, 200, 3))
+#
+#     if K.image_data_format() == 'channels_last':
+#         bn_axis = 3
+#     else:
+#         bn_axis = 1
+#
+#     x = Conv2D(
+#         64, (7, 7), strides=(2, 2), padding='same', name='conv1')(img_input)
+#     x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+#     x = Activation('relu')(x)
+#     x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+#
+#     x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
+#     x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
+#     x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
+#
+#     x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
+#     x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
+#     x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
+#     x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
+#
+#     # x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
+#     # x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
+#     # x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
+#     # x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
+#     # x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
+#     # x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
+#     #
+#     # x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+#     # x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+#     # x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+#
+#     x = AveragePooling2D((7, 7), name='avg_pool')(x)
+#
+#     x = Flatten()(x)
+#     out = Dense(NUM_PARAMS, activation='linear', name='fc1000')(x)
+#
+#     model = Model(img_input, out, name='resnet50')
+#     # model = Model(input_shape, out)
+#
+#     if CONTINUE:
+#         model.load_weights(DATA_DIR + "/interaction_weights_"+str(NUM_PARAMS)+".h5")
+#
+#     model.summary()
+#     model.compile(loss='mean_squared_error',
+#                   optimizer='adam')
+#
+#     return model
+
+
+
+# def model():
+#     global NUM_PARAMS, DATA_DIR, CONTINUE
+#     input_shape = Input(shape=(200, 200, 3))
+#
+#     # LOAD...
+#     from keras.models import model_from_json
+#
+#     json_file = open(ROOT_DIR+'/vision_model_'+WEIGHTS+'.json', 'r')
+#     vision_model_json = json_file.read()
+#     json_file.close()
+#     vision_model = model_from_json(vision_model_json)
+#     # load weights into new model
+#     vision_model.load_weights(ROOT_DIR+"/vision_"+WEIGHTS+".h5")
+#     # vision_model.layers.pop()
+#     # vision_model.layers.pop()
+#
+#     vision_model.summary()
+#
+#     # The vision model will be shared, weights and all
+#     out_a = vision_model(input_shape)
+#     # out_a = Flatten()(out_a)
+#     #
+#     # out_a = Dense(256, activation='relu')(out_a)
+#     # out_a = Dense(128, activation='relu')(out_a)
+#     # out_a = Dense(32, activation='relu')(out_a)
+#     out_a = Dense(64, activation='relu')(out_a)
+#     out_a = Dense(32, activation='relu')(out_a)
+#
+#     # out = Dense(128, activation='relu')(out_a)
+#     # out = Dense(K, activation='softmax')(out_a)
+#     previous = NUM_PARAMS - 2
+#     if NUM_PARAMS == 4 or CONTINUE:
+#         previous = NUM_PARAMS
+#
+#     out = Dense(previous, kernel_initializer='normal', activation='linear')(out_a)
+#
+#     model = Model(input_shape, out)
+#
+#     model.load_weights(DATA_DIR + "/interaction_weights_"+str(previous)+".h5")
+#     # model.load_weights(DATA_DIR + "/interaction_weights.h5")
+#
+#     out = Dense(NUM_PARAMS, kernel_initializer='normal', activation='linear')(out_a)
+#     model = Model(input_shape, out)
+#     #
+#     # model =
+#
+#     model.summary()
+#     # 8. Compile model
+#     # adam = Adam(lr=0.005, beta_1=0.9, beta_2=0.999, epsilon=1e-8)
+#     model.compile(loss='mean_squared_error',
+#                   optimizer='adam')
+#
+#     # model.lr.set_value(0.05)
+#
+#     return model
 
 
 if __name__ == '__main__':
