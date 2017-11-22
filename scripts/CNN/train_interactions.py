c9e8a774222f14edad54a899477716982678d090
Filip Naiser <filip@naiser.cz>
2017-11-22 20:46:46 +0100

    resnet

--- scripts/CNN/train_interactions.py
+++ scripts/CNN/train_interactions.py
@@ -9,7 +9,9 @@ from keras.utils import np_utils
 from keras.layers import Conv2D, MaxPooling2D, Input, Dense, Flatten
 from keras.models import Model
 from keras.optimizers import Adam
-from keras.layers import Dense
+from keras.layers import Dense, BatchNormalization, Activation, AveragePooling2D
+from keras import backend as K
+from keras import layers
 from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
@@ -24,34 +26,130 @@ TWO_TESTS = True
 WEIGHTS = 'cam3_zebr_weights_vgg'
 NUM_PARAMS = 6
 
+def identity_block(input_tensor, kernel_size, filters, stage, block):
+    """The identity block is the block that has no conv layer at shortcut.
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    # Returns
+        Output tensor for the block.
+    """
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(filters1, (1, 1), name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters2, kernel_size,
+               padding='same', name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    x = layers.add([x, input_tensor])
+    x = Activation('relu')(x)
+    return x
+
+
+def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2)):
+    """A block that has a conv layer at shortcut.
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    # Returns
+        Output tensor for the block.
+    Note that from stage 3, the first conv layer at main path is with strides=(2,2)
+    And the shortcut should have strides=(2,2) as well
+    """
+    filters1, filters2, filters3 = filters
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Conv2D(filters1, (1, 1), strides=strides,
+               name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters2, kernel_size, padding='same',
+               name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    shortcut = Conv2D(filters3, (1, 1), strides=strides,
+                      name=conv_name_base + '1')(input_tensor)
+    shortcut = BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+    x = layers.add([x, shortcut])
+    x = Activation('relu')(x)
+    return x
+
+
+
+
 def model():
     global NUM_PARAMS, DATA_DIR, CONTINUE
 
-    input_shape = Input(shape=(200, 200, 3))
+    img_input = Input(shape=(200, 200, 3))
 
-    x = Conv2D(32, (3, 3))(input_shape)
-    x = Conv2D(32, (3, 3))(x)
-    x = Conv2D(32, (15, 15), dilation_rate=(2, 2))(x)
-    x = MaxPooling2D((2, 2))(x)
-    x = Conv2D(32, (5, 5), dilation_rate=(2, 2))(x)
-    # x = Conv2D(32, (3, 3))(x)
-    # x = Conv2D(32, (3, 3))(x)
-    # x = MaxPooling2D((2, 2))(x)
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
 
-    # x = Conv2D(64, (3, 3))(x)
-    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
-    x = Conv2D(32, (3, 3))(x)
-    x = MaxPooling2D((2, 2))(x)
-    x = Conv2D(16, (3, 3))(x)
+    x = Conv2D(
+        64, (7, 7), strides=(2, 2), padding='same', name='conv1')(img_input)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
+
+    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
+
+    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
+
+    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+
+    x = AveragePooling2D((7, 7), name='avg_pool')(x)
+
     x = Flatten()(x)
-    x = Dense(256)(x)
-    x = Dense(128)(x)
+    out = Dense(NUM_PARAMS, activation='linear', name='fc1000')(x)
 
-
-
-    out = Dense(NUM_PARAMS, activation='linear')(x)
-
-    model = Model(input_shape, out)
+    model = Model(img_input, out, name='resnet50')
+    # model = Model(input_shape, out)
 
     if CONTINUE:
         model.load_weights(DATA_DIR + "/interaction_weights_"+str(NUM_PARAMS)+".h5")
@@ -124,7 +222,7 @@ def model():
 if __name__ == '__main__':
     NUM_EPOCHS = 5
     USE_PREVIOUS_AS_INIT = 0
-    K = 6
+    # K = 6
     WEIGHTS = 'best_weights'
     CONTINUE = False
     SAMPLES = 2000
