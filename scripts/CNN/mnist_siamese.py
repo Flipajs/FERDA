89da521d06405cb3e532056618f25fa86e87693e
Filip Naiser <filip@naiser.cz>
2017-12-19 23:04:26 +0100

    experiments with different architectures for siamese metric learning / embedding...

--- scripts/CNN/mnist_siamese.py
+++ scripts/CNN/mnist_siamese.py
@@ -1,3 +1,5 @@
+# based on https://github.com/keras-team/keras/blob/master/examples/mnist_siamese.py
+
 '''Trains a Siamese MLP on pairs of digits from the MNIST dataset.
 
 It follows Hadsell-et-al.'06 [1] by computing the Euclidean distance on the
@@ -43,26 +45,30 @@ def contrastive_loss(y_true, y_pred):
     return K.mean(y_true * K.square(y_pred) +
                   (1 - y_true) * K.square(K.maximum(margin - y_pred, 0)))
 
-def create_base_network(input_shape):
+def create_base_network1(input_shape):
     '''Base network to be shared (eq. to feature extraction).
     '''
     input = Input(shape=input_shape)
 
-    # x = Conv2DReluBatchNorm(32, 3, 3, animal_input)
-    x = Conv2D(32, (3, 3), activation='relu')(input)
-    x = Conv2D(16, (3, 3), activation='relu', dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3))(input)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
     x = MaxPooling2D((2, 2))(x)
-    # x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
-    # x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
-    # x = Conv2DReluBatchNorm(32, 3, 3, x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = Conv2D(32, (3, 3))(x)
     # x = MaxPooling2D((2, 2))(x)
-    # x = Conv2DReluBatchNorm(16, 3, 3, x)
-    x = Conv2D(8, (3, 3), activation='relu')(x)
-    # x = Conv2DReluBatchNorm(8, 3, 3, x)
-    x = Conv2D(8, (3, 3), activation='relu')(x)
-    x = Conv2D(8, (3, 3), activation='relu')(x)
-    x = Flatten()(x)
 
+    # x = Conv2D(64, (3, 3))(x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3))(x)
+    # x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(8, (3, 3))(x)
+    # out_a = vision_model(animal_a)
+    x = Flatten()(x)
+    # out = Dense(128, activation='relu')(out_a)
+    # out = Dense(K, activation='softmax')(x)
 
     # x = Flatten()(input)
     # x = Dense(128, activation='relu')(x)
@@ -73,6 +79,207 @@ def create_base_network(input_shape):
     m = Model(input, x)
     m.summary()
     return m
+
+def create_base_network2(input_shape):
+    '''Base network to be shared (eq. to feature extraction).
+    '''
+    input = Input(shape=input_shape)
+
+    x = Conv2D(32, (3, 3))(input)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = MaxPooling2D((2, 2))(x)
+
+    # x = Conv2D(64, (3, 3))(x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3))(x)
+    # x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(8, (3, 3))(x)
+    # out_a = vision_model(animal_a)
+    x = Flatten()(x)
+    # out = Dense(128, activation='relu')(out_a)
+    # out = Dense(K, activation='softmax')(x)
+
+    # x = Flatten()(input)
+    # x = Dense(128, activation='relu')(x)
+    # x = Dropout(0.1)(x)
+    # x = Dense(128, activation='relu')(x)
+    # x = Dropout(0.1)(x)
+    x = Dense(128, activation='relu')(x)
+    m = Model(input, x)
+    m.summary()
+    return m
+
+def create_base_network3(input_shape):
+    '''Base network to be shared (eq. to feature extraction).
+    '''
+    input = Input(shape=input_shape)
+
+    x = Conv2D(32, (3, 3))(input)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = MaxPooling2D((2, 2))(x)
+
+    # x = Conv2D(64, (3, 3))(x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3))(x)
+    x = Conv2D(32, (3, 3))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Flatten()(x)
+
+    x = Dense(128, activation='relu')(x)
+    m = Model(input, x)
+    m.summary()
+    return m
+
+def create_base_network4(input_shape):
+    '''Base network to be shared (eq. to feature extraction).
+    '''
+    input = Input(shape=input_shape)
+
+    x = Conv2D(32, (3, 3))(input)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = MaxPooling2D((2, 2))(x)
+
+    # x = Conv2D(64, (3, 3))(x)
+    x = Conv2D(32, (3, 3), dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3))(x)
+    x = Conv2D(32, (3, 3))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Flatten()(x)
+
+    x = Dense(128, activation='linear')(x)
+    m = Model(input, x)
+    m.summary()
+    return m
+
+def create_base_network5(input_shape):
+    '''Base network to be shared (eq. to feature extraction).
+    '''
+    input = Input(shape=input_shape)
+
+    x = Conv2D(32, (3, 3), activation='relu')(input)
+    x = Conv2D(32, (3, 3), activation='relu', dilation_rate=(2, 2))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), activation='relu', dilation_rate=(2, 2))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = MaxPooling2D((2, 2))(x)
+
+    # x = Conv2D(64, (3, 3))(x)
+    x = Conv2D(32, (3, 3), activation='relu', dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3), activation='relu')(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), activation='relu')(x)
+    x = Conv2D(32, (3, 3), activation='relu')(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Flatten()(x)
+
+    x = Dense(128, activation='linear')(x)
+    m = Model(input, x)
+    m.summary()
+    return m
+
+def create_base_network6(input_shape):
+    '''Base network to be shared (eq. to feature extraction).
+    '''
+    input = Input(shape=input_shape)
+
+    x = Conv2D(32, (3, 3), activation='relu')(input)
+    x = Conv2D(32, (3, 3), activation='relu', dilation_rate=(2, 2))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), activation='relu', dilation_rate=(2, 2))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = MaxPooling2D((2, 2))(x)
+
+    # x = Conv2D(64, (3, 3))(x)
+    x = Conv2D(32, (3, 3), activation='relu', dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3), activation='relu')(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), activation='relu')(x)
+    x = Conv2D(32, (3, 3), activation='relu')(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Flatten()(x)
+
+    x = Dense(32, activation='relu')(x)
+    x = Dense(32, activation='linear')(x)
+    m = Model(input, x)
+    m.summary()
+    return m
+
+def create_base_network7(input_shape):
+    '''Base network to be shared (eq. to feature extraction).
+    '''
+    input = Input(shape=input_shape)
+
+    x = Conv2D(32, (3, 3), activation='relu', padding='same')(input)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same', dilation_rate=(2, 2))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same', dilation_rate=(2, 2))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = MaxPooling2D((2, 2))(x)
+
+    # x = Conv2D(64, (3, 3))(x)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same', dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Flatten()(x)
+
+    x = Dense(32, activation='relu')(x)
+    x = Dense(32, activation='linear')(x)
+    m = Model(input, x)
+    m.summary()
+    return m
+
+
+def create_base_network8(input_shape):
+    ''' Basiacly network 5 + padding added...
+    '''
+    input = Input(shape=input_shape)
+
+    x = Conv2D(32, (3, 3), padding='same', activation='relu')(input)
+    x = Conv2D(32, (3, 3), padding='same', activation='relu', dilation_rate=(2, 2))(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), padding='same', activation='relu', dilation_rate=(2, 2))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = Conv2D(32, (3, 3))(x)
+    # x = MaxPooling2D((2, 2))(x)
+
+    # x = Conv2D(64, (3, 3))(x)
+    x = Conv2D(32, (3, 3), padding='same', activation='relu', dilation_rate=(2, 2))(x)
+    x = Conv2D(32, (3, 3), padding='same', activation='relu')(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Conv2D(32, (3, 3), padding='same', activation='relu')(x)
+    x = Conv2D(32, (3, 3), padding='same', activation='relu')(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Flatten()(x)
+
+    x = Dense(128, activation='linear')(x)
+    m = Model(input, x)
+    m.summary()
+    return m
+
 
 
 def compute_accuracy(y_true, y_pred):
@@ -99,10 +306,10 @@ if __name__ == '__main__':
                         default='/Users/flipajs/Documents/wd/FERDA/CNN_hard_datagen',
                         help='path to dataset')
     parser.add_argument('--epochs', type=int,
-                        default=5,
+                        default=10,
                         help='number of epochs')
     parser.add_argument('--batch_size', type=int,
-                        default=32,
+                        default=128,
                         help='batch size')
     parser.add_argument('--weights_name', type=str, default='best_weights',
                         help='name used for saving intermediate results')
@@ -132,35 +339,51 @@ if __name__ == '__main__':
     input_shape = tr_pairs.shape[2:]
     print(input_shape)
 
-    # network definition
-    base_network = create_base_network(input_shape)
+    architectures = [
+                     # create_base_network1,
+                     # create_base_network2,
+                     # create_base_network3,
+                     # create_base_network4,
+                     # create_base_network5,
+                     # create_base_network6,
+                     # create_base_network7,
+                     create_base_network8,
+                     ]
 
-    input_a = Input(shape=input_shape)
-    input_b = Input(shape=input_shape)
+    for architecture in architectures:
+        print("")
+        print("")
+        print("###################################")
+        print(architecture)
+        # network definition
+        base_network = architecture(input_shape)
 
-    # because we re-use the same instance `base_network`,
-    # the weights of the network
-    # will be shared across the two branches
-    processed_a = base_network(input_a)
-    processed_b = base_network(input_b)
+        input_a = Input(shape=input_shape)
+        input_b = Input(shape=input_shape)
 
-    distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
+        # because we re-use the same instance `base_network`,
+        # the weights of the network
+        # will be shared across the two branches
+        processed_a = base_network(input_a)
+        processed_b = base_network(input_b)
 
-    model = Model([input_a, input_b], distance)
-    model.summary()
-    # train
-    # rms = RMSprop()
-    model.compile(loss=contrastive_loss, optimizer='adam', metrics=[accuracy])
-    model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
-              batch_size=128,
-              epochs=args.epochs,
-              validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y))
+        distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
 
-    # compute final accuracy on training and test sets
-    y_pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
-    tr_acc = compute_accuracy(tr_y, y_pred)
-    y_pred = model.predict([te_pairs[:, 0], te_pairs[:, 1]])
-    te_acc = compute_accuracy(te_y, y_pred)
+        model = Model([input_a, input_b], distance)
+        model.summary()
+        # train
+        # rms = RMSprop()
+        model.compile(loss=contrastive_loss, optimizer='adam', metrics=[accuracy])
+        model.fit([tr_pairs[:, 0], tr_pairs[:, 1]], tr_y,
+                  batch_size=args.batch_size,
+                  epochs=args.epochs,
+                  validation_data=([te_pairs[:, 0], te_pairs[:, 1]], te_y))
 
-    print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
-    print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
+        # compute final accuracy on training and test sets
+        y_pred = model.predict([tr_pairs[:, 0], tr_pairs[:, 1]])
+        tr_acc = compute_accuracy(tr_y, y_pred)
+        y_pred = model.predict([te_pairs[:, 0], te_pairs[:, 1]])
+        te_acc = compute_accuracy(te_y, y_pred)
+
+        print('* Accuracy on training set: %0.2f%%' % (100 * tr_acc))
+        print('* Accuracy on test set: %0.2f%%' % (100 * te_acc))
