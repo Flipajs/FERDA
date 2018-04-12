1d1e61a311962db31f56d11305e54b401030c451
Matěj Šmíd <m@matejsmid.cz>
2018-04-12 11:35:03 +0200

    parallelization: command line parameters, sequential processing

--- core/parallelization.py
+++ core/parallelization.py
@@ -24,6 +24,9 @@ import numpy as np
 import time
 import cv2
 from core.region.mser_operations import children_filter
+import fire
+import subprocess
+import tqdm
 
 
 def segment(proj, img):
@@ -39,12 +42,14 @@ def segment(proj, img):
 
     return result
 
+
 def prepare_img(proj, img):
     grayscale = True
     if hasattr(proj, 'segmentation_model') and proj.segmentation_model is not None:
         grayscale = False
 
     return prepare_for_segmentation(img, proj, grayscale)
+
 
 def check_intersection(rois, roi):
     intersect = -1
@@ -54,6 +59,7 @@ def check_intersection(rois, roi):
             return i
 
     return intersect
+
 
 def get_rois(msers, img, prediction_optimisation_border):
     rois = []
@@ -77,29 +83,53 @@ def get_rois(msers, img, prediction_optimisation_border):
 
     return rois
 
-if __name__ == '__main__':
-    print sys.argv
-    working_dir = sys.argv[1]
-    proj_name = sys.argv[2]
-    id = int(sys.argv[3])
-    frames_in_row = int(sys.argv[4])
-    last_n_frames = int(sys.argv[5])
 
+def run_parallelization(working_dir, proj_name, id=None, frames_in_row=None, last_n_frames=None, sge=False):
+    # /home/matej/prace/ferda/projects/after_par/5Zebrafish_nocover_22min 5Zebrafish_nocover_22min 0 100 0
+    if id is None and frames_in_row is None and last_n_frames is None:
+        try:
+            with open(os.path.join(working_dir, 'limits.txt'), 'r') as fr:
+                # the file should look like:
+                # 0	100	0
+                # 1	100	0
+                # 2	100	0
+                # ...
+                parameters = []
+                for line in fr.readlines():
+                    split = line.split()
+                    parameters.append({'id': int(split[0]),
+                                       'frames_in_row': int(split[1]),
+                                       'last_n_frames': int(split[2])})
+            for par in tqdm.tqdm(parameters, desc='processing segmentation jobs in sequence'):
+                if sge:
+                    assert False, 'not implemented'
+                    subprocess.check_call(
+                        ['scripts/sun_grid_engine_parallelization/compute_single_id.sh',
+                         working_dir, proj_name, par['id'], par['frames_in_row'], par['last_n_frames']
+                         ])
+                else:
+                    compute_single_id(working_dir, proj_name, par['id'], par['frames_in_row'], par['last_n_frames'])
+
+        except IOError as e:
+            print('Can''t open limits.txt file: {}'.format(e))
+            raise
+    else:
+        assert id is not None and frames_in_row is not None and last_n_frames is not None
+        compute_single_id(working_dir, proj_name, id, frames_in_row, last_n_frames)
+
+
+def compute_single_id(working_dir, proj_name, id, frames_in_row, last_n_frames):
     # check if part was computed before
     if os.path.isfile(working_dir + '/temp/part' + str(id) + '_rm.sqlite3'):
         if os.stat(working_dir + '/temp/part' + str(id) + '_rm.sqlite3') != 0:
             if os.path.isfile(working_dir + '/temp/part' + str(id) + '.pkl'):
                 if os.stat(working_dir + '/temp/part' + str(id) + '.pkl') != 0:
-                    import sys
-
-                    sys.exit("Part already processed")
-
+                    print('Part {} already processed'.format(id))
+                    return
     proj = Project()
-    proj.load(working_dir+'/'+proj_name+'.fproj')
-
+    proj.load(working_dir + '/' + proj_name + '.fproj')
     # proj.stats.major_axis_median = 18
     # proj.solver_parameters.max_edge_distance_in_ant_length = 2
-
     try:
         use_roi_prediction_optimisation = proj.other_parameters.segmentation_use_roi_prediction_optimisation
 
@@ -110,74 +140,65 @@ if __name__ == '__main__':
         use_roi_prediction_optimisation = True  # obsolete, per-pixel classification
         prediction_optimisation_border = 25
         full_segmentation_refresh = 25
-
-    if not os.path.exists(proj.working_directory+'/temp'):
+    if not os.path.exists(proj.working_directory + '/temp'):
         try:
-            os.mkdir(proj.working_directory+'/temp')
+            os.mkdir(proj.working_directory + '/temp')
         except OSError:
             pass
-
     if not proj.is_cluster():
-        temp_local_path = proj.working_directory+'/temp'
+        temp_local_path = proj.working_directory + '/temp'
     else:
-        temp_local_path='/localhome/casillas/'
+        temp_local_path = '/localhome/casillas/'
 
-        if not os.path.exists(temp_local_path+proj_name):
+        if not os.path.exists(temp_local_path + proj_name):
             try:
-                os.mkdir(temp_local_path+proj_name)
+                os.mkdir(temp_local_path + proj_name)
             except:
-                print(temp_local_path+proj_name + "   was created between check and mkdir")
+                print(temp_local_path + proj_name + "   was created between check and mkdir")
 
-        temp_local_path=temp_local_path + proj_name
+        temp_local_path = temp_local_path + proj_name
 
-        if not os.path.exists(temp_local_path+'/temp'):
+        if not os.path.exists(temp_local_path + '/temp'):
             try:
-                os.mkdir(temp_local_path+'/temp')
+                os.mkdir(temp_local_path + '/temp')
             except:
-                print(temp_local_path+'/temp' + "   was created between check and mkdir")
+                print(temp_local_path + '/temp' + "   was created between check and mkdir")
 
-        temp_local_path=temp_local_path+'/temp'
+        temp_local_path = temp_local_path + '/temp'
 
     # init managers
     solver = Solver(proj)
     from core.graph.graph_manager import GraphManager
     proj.gm = GraphManager(proj, proj.solver.assignment_score)
     # TODO: add global params
-    proj.rm = RegionManager(db_wd=temp_local_path, db_name='part'+str(id)+'_rm.sqlite3', cache_size_limit=10000)
+    proj.rm = RegionManager(db_wd=temp_local_path, db_name='part' + str(id) + '_rm.sqlite3', cache_size_limit=10000)
     proj.chm = ChunkManager()
     proj.color_manager = None
-
     S_.general.log_graph_edits = False
-
     vid = get_auto_video_manager(proj)
-    if id*frames_in_row > 0:
-        img = vid.seek_frame(id*frames_in_row)
+    if id * frames_in_row > 0:
+        img = vid.seek_frame(id * frames_in_row)
     else:
         img = vid.next_frame()
-
     if img is None:
-        raise Exception("img is None, there is something wrong with frame: "+str(id*frames_in_row))
-
+        raise Exception("img is None, there is something wrong with frame: " + str(id * frames_in_row))
     rois = []
     img = prepare_img(proj, img)
-
     msers_t = 0
     solver_t = 0
     vid_t = 0
     file_t = 0
-
     border2 = 3
-
     jj = 0
     # for all frames: extract regions and add them to the graph
     for i in range(frames_in_row + last_n_frames):
-        frame = id*frames_in_row + i
+        frame = id * frames_in_row + i
 
         s = time.time()
 
         # per pixel classification -> fg, bg (not used)
         if hasattr(proj, 'segmentation_model'):
-            if rois and i%full_segmentation_refresh != 0:
+            if rois and i % full_segmentation_refresh != 0:
                 try:
                     t = time.time()
 
@@ -203,7 +224,7 @@ if __name__ == '__main__':
                         # add border2 (to prevent segmentation artefacts
                         crop = cv2.copyMakeBorder(crop, border2, border2, border2, border2, cv2.BORDER_REPLICATE)
 
-                        t2_  = time.time()
+                        t2_ = time.time()
                         proj.segmentation_model.set_image(crop)
                         # t2_ = time.time() - t2_
                         # t2 += t2_
@@ -229,8 +250,8 @@ if __name__ == '__main__':
 
                         new_im[h1:h2, w1:w2] = seg_img.copy()
 
-
-                    print "segmentation time: {:.3f}, #roi: {} roi area: {} roi coverage: {:.3f}".format(time.time() - t, len(rois), area, area / float(img.shape[0] * img.shape[1]))
+                    print "segmentation time: {:.3f}, #roi: {} roi area: {} roi coverage: {:.3f}".format(
+                        time.time() - t, len(rois), area, area / float(img.shape[0] * img.shape[1]))
                     # t = time.time()
                     # segment(proj, img)
                     # print "without ", time.time() - t
@@ -248,12 +269,12 @@ if __name__ == '__main__':
             proj.colormarks_model.assign_colormarks(proj, msers)
 
         proj.rm.add(msers)
-        msers_t += time.time()-s
+        msers_t += time.time() - s
 
         s = time.time()
 
         # Check for last frame...
-        if i+1 < frames_in_row + last_n_frames:
+        if i + 1 < frames_in_row + last_n_frames:
             img = vid.next_frame()
             if img is None:
                 raise Exception("img is None, there is something wrong with frame: " + str(frame))
@@ -273,10 +294,8 @@ if __name__ == '__main__':
 
     # if proj.solver_parameters.use_emd_for_split_merge_detection():
     #     solver.detect_split_merge_cases()
-
     s = time.time()
     print "#Edges BEFORE: ", proj.gm.g.num_edges()
-
     try:
         # TODO:
         if proj.type == 'colony':
@@ -289,26 +308,25 @@ if __name__ == '__main__':
             solver.simplify(rules=[solver.one2one])
     except:
         solver.one2one()
-
     print "#Edges AFTER: ", proj.gm.g.num_edges()
-
     solver_t += time.time() - s
-
     s = time.time()
-
-    with open(proj.working_directory+'/temp/part'+str(id)+'.pkl', 'wb') as f:
+    with open(proj.working_directory + '/temp/part' + str(id) + '.pkl', 'wb') as f:
         p = pickle.Pickler(f, -1)
         p.dump(proj.gm.g)
         p.dump(proj.gm.get_all_relevant_vertices())
         p.dump(proj.chm)
-
     file_t = time.time() - s
-
     print "#Vertices: {}, #Edges: {}".format(proj.gm.g.num_vertices(), proj.gm.g.num_edges())
-    print "MSERS t:", round(msers_t, 2), "SOLVER t: ", round(solver_t, 2), "VIDEO t:", round(vid_t, 2), "FILE t: ", round(file_t, 2), "SUM t / frames_in_row:", round((msers_t + solver_t+vid_t+file_t)/float(frames_in_row), 2)
-
+    print "MSERS t:", round(msers_t, 2), "SOLVER t: ", round(solver_t, 2), "VIDEO t:", round(vid_t,
+                                                                                             2), "FILE t: ", round(
+        file_t, 2), "SUM t / frames_in_row:", round((msers_t + solver_t + vid_t + file_t) / float(frames_in_row), 2)
     if proj.is_cluster():
         import shutil
         import glob
         for file in glob.glob(temp_local_path + '/part' + str(id) + '_rm.sqlite3'):
             shutil.move(file, working_dir + '/temp')
+
+
+if __name__ == '__main__':
+    fire.Fire(run_parallelization)
