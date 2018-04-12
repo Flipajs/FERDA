5010e3f0397beb692e78670cd37c4fbea0d9c060
Matěj Šmíd <m@matejsmid.cz>
2018-04-12 11:04:00 +0200

    ferda command line argument: load project

--- main.py
+++ main.py
@@ -1,62 +1,30 @@
 import sys
 import cPickle as pickle
 from PyQt4 import QtGui
-
 from gui import main_window
 from core.settings import Settings as S_
-from utils.misc import is_flipajs_pc, is_matejs_pc
 import time
 from core.project.project import Project
 import timeit
 import numpy as np
+import argparse
 
+parser = argparse.ArgumentParser(description='FERDA laboratory animal tracking system.')
+parser.add_argument('project', nargs='?', help='project directory or file')
+args = parser.parse_args()
 
 app = QtGui.QApplication(sys.argv)
 ex = main_window.MainWindow()
 ex.setFocus()
 
 t_ = time.time()
-
-project = Project()
-
 S_.general.print_log = False
+if args.project is not None:
+    project = Project()
+    project.load(args.project)
 
-# This is development speed up process (kind of fast start). Runs only on developers machines...
-# if is_flipajs_pc() and False:
-wd = None
-if is_flipajs_pc():
-    # wd = '/Users/flipajs/Documents/wd/FERDA/Cam1_rf'
-    # wd = '/Users/flipajs/Documents/wd/FERDA/Cam1_playground'
-    # wd = '/Users/flipajs/Documents/wd/FERDA/test6'
-    # wd = '/Users/flipajs/Documents/wd/FERDA/zebrafish_playground'
-    # wd = '/Users/flipajs/Documents/wd/FERDA/zebrafish_new'
-    # wd = '/Users/flipajs/Documents/wd/FERDA/Camera3'
-    # wd = '/Users/flipajs/Documents/wd/FERDA/Cam1_rfs2'
-    # wd = '/Users/flipajs/Documents/wd/FERDA/Cam1_assembly'
-    wd = '/Users/flipajs/Documents/wd/FERDA/Cam1'
-    # wd = '/Volumes/Seagate Expansion Drive/HH1_PRE_upper_thr_'
-    # wd = '/Volumes/Seagate Expansion Drive/HH1_PRE'
-    # wd = '/Volumes/Seagate Expansion Drive/HH1_POST'
-    # wd = '/Users/flipajs/Documents/wd/FERDA/Barbara_flu_bug/test6'
-    # wd = '/Users/flipajs/Documents/wd/FERDA/rep1-cam2'
-    # wd = '/Users/flipajs/Documents/wd/FERDA/rep1-cam3'
-
-    # wd = '/Users/flipajs/Documents/wd/FERDA/Sowbug3'
-    # wd = '/Users/flipajs/Documents/wd/FERDA/Sowbug3_new'
-
-    # wd = '/Users/flipajs/Documents/wd/FERDA/test'
-
-if is_matejs_pc():
-    # wd = '/home/matej/prace/ferda/10-15/'
-    # wd = '/home/matej/prace/ferda/10-15 (copy)/'
-    pass
-
-if wd is not None:
-    project.load(wd)
-
-
-    for t in project.chm.chunk_gen():
-        print "Cardinality: {}, t_id: {}".format(t.get_cardinality(project.gm), t.id())
+    # for t in project.chm.chunk_gen():
+    #     print "Cardinality: {}, t_id: {}".format(t.get_cardinality(project.gm), t.id())
 
     # from tqdm import tqdm
     # thetas = []
@@ -67,10 +35,8 @@ if wd is not None:
     #
     # plt.hist(thetas)
     # plt.show()
-
     # TODO !! add it to assembly process
     # project.solver.one2one(check_tclass=True)
-
     # cases = []
     # for v in project.gm.g.vertices():
     #     if v.in_degree() == 2:
@@ -87,10 +53,9 @@ if wd is not None:
     #                 cases.append(t)
     #
     # print "#CASES: {}".format(len(cases))
-    from core.region.fitting import Fitting
-    from tqdm import tqdm
-    from scipy.ndimage.morphology import binary_erosion, binary_dilation
-
+    #from core.region.fitting import Fitting
+    #from tqdm import tqdm
+    #from scipy.ndimage.morphology import binary_erosion, binary_dilation
     # erosion = True
     #
     # ii = 0
@@ -158,19 +123,19 @@ if wd is not None:
     #             new_animals.append(r)
     #
     #         animals = new_animals
-
-        # from scripts.regions_stats import decide_one2one
-        # decide_one2one(project)
-
-        # project.chm.add_single_vertices_chunks(project)
-        # project.save()
-
-        # project.load_semistate(wd, 'edge_cost_updated', update_t_nodes=True)
-        # project.load_semistate(wd, 'first_tracklets')
-        # project.load_semistate(wd, 'lp_id_SEG_IDCR_0')
-        # project.load_semistate(wd, 'lp_HIL_INIT3_0')
-
-        # project.gm.update_nodes_in_t_refs()
+    #
+    # from scripts.regions_stats import decide_one2one
+    # decide_one2one(project)
+    #
+    # project.chm.add_single_vertices_chunks(project)
+    # project.save()
+    #
+    # project.load_semistate(wd, 'edge_cost_updated', update_t_nodes=True)
+    # project.load_semistate(wd, 'first_tracklets')
+    # project.load_semistate(wd, 'lp_id_SEG_IDCR_0')
+    # project.load_semistate(wd, 'lp_HIL_INIT3_0')
+    #
+    # project.gm.update_nodes_in_t_refs()
 
     try:
         # old projects WORKAROUND:
@@ -182,8 +147,6 @@ if wd is not None:
         pass
 
     ex.widget_control('load_project', project)
-    # colorize_project(project)
-    # ex.move(-500, -500)
     ex.showMaximized()
 
 print "FERDA is READY, loaded in {:.3}s".format(time.time()-t_)
