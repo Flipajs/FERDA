reidentification small fixes, descriptors computation added to cli

scripts/CNN/prepare_siamese_data.py
@@ -1,11 +1,25 @@
+from __future__ import print_function
+from __future__ import print_function
 import numpy as np
-from utils.img import get_safe_selection
-from utils.img import apply_ellipse_mask
 import tqdm
 from cachetools import LRUCache
-from random import randint
 from tqdm import tqdm
-from random import randint, choice
+import random
+import h5py
+from os.path import join
+from core.project.project import Project
+from utils.img import get_safe_selection
+from utils.img import apply_ellipse_mask
+
+# TODO: project parameter?
+OFFSET = 45
+ELLIPSE_DILATION = 10
+MASK_SIGMA = 10
+BATCH_SIZE = 500
+APPLY_ELLIPSE = True
+ALIGN = True
+NUM_EXAMPLES = 5000
+TEST_RATIO = .1
 
 
 def get_region_image(region, vm, offset=45, add_ellipse_mask=True, mask_sigma=10, ellipse_dilation=10, align=True):
@@ -24,45 +38,19 @@ def get_region_crop(region, img, add_ellipse_mask, ellipse_dilation, mask_sigma,
         crop = apply_ellipse_mask(region, crop, mask_sigma, ellipse_dilation)
 
     if align:
-        flip = 180*randint(0, 1)
+        flip = 180*random.randint(0, 1)
         import imutils
         angle = int(region.theta_ * 57.295 + flip)
         crop = imutils.rotate(crop, -angle)
 
     return crop
 
-# TODO: project parameter?
-OFFSET = 45
-ELLIPSE_DILATION = 10
-MASK_SIGMA = 10
-BATCH_SIZE = 500
-APPLY_ELLIPSE = True
-ALIGN = True
 
-if __name__ == '__main__':
-    NUM_EXAMPLES = 5000
-    # P_WD = '/Users/flipajs/Documents/wd/FERDA/april-paper/Cam1_clip'
-    # P_WD = '/Users/flipajs/Documents/wd/FERDA/april-paper/Sowbug3-crop'
-    P_WD = '/Users/flipajs/Documents/wd/FERDA/april-paper/5Zebrafish_nocover_22min'
-    P_WD = '/Users/flipajs/Documents/wd/FERDA/april-paper/Camera3-5min'
-    # P_WD = '/Users/flipajs/Documents/wd/FERDA/zebrafish_new'
-    from core.project.project import Project
-
-    p = Project()
-    p.load(P_WD)
-
-    imgs_batch = []
-    ids_batch = []
-    descriptors = {}
-
-    import cv2
-
-    TEST_RATIO = .1
-
+def generate_reidentification_training_data(project_path, out_dir):
     LINEAR = False
-
-
     np.set_printoptions(precision=2)
+
+    p = Project(project_path)
     vm = p.get_video_manager()
     cache = LRUCache(maxsize=5000, missing=lambda x: vm.get_frame(x))
 
@@ -70,41 +58,26 @@ if __name__ == '__main__':
         for i in tqdm(range(p.num_frames())):
             cache[i]
 
-    from random import randint, choice
-
     imgs = []
     labels = []
-
     i = 0
-
     with tqdm(total=NUM_EXAMPLES) as pbar:
         while i < NUM_EXAMPLES:
-            frame = randint(0, vm.total_frame_count())
+            frame = random.randint(0, vm.total_frame_count())
 
             tracklets = filter(lambda x: x.is_single(), p.chm.tracklets_in_frame(frame))
             if len(tracklets) > 1:
-                trackletA = choice(tracklets)
+                trackletA, trackletB = random.sample(tracklets)
                 regionA1 = trackletA.get_random_region(p.gm)
                 regionA2 = trackletA.get_random_region(p.gm)
-
-                # TODO: is there a more elegant way?
-                while True:
-                    trackletB = choice(tracklets)
-                    if trackletB != trackletA:
-                        break
-
                 regionB = trackletB.get_random_region(p.gm)
 
-                cropA1 = get_region_image(regionA1, vm, offset=OFFSET, add_ellipse_mask=APPLY_ELLIPSE, mask_sigma=MASK_SIGMA, ellipse_dilation=ELLIPSE_DILATION, align=ALIGN)
+                cropA1 = get_region_image(regionA1, vm, offset=OFFSET, add_ellipse_mask=APPLY_ELLIPSE,
+                                          mask_sigma=MASK_SIGMA, ellipse_dilation=ELLIPSE_DILATION, align=ALIGN)
                 cropA2 = get_region_image(regionA2, vm, offset=OFFSET, add_ellipse_mask=APPLY_ELLIPSE,
                                           mask_sigma=MASK_SIGMA, ellipse_dilation=ELLIPSE_DILATION, align=ALIGN)
                 cropB = get_region_image(regionB, vm, offset=OFFSET, add_ellipse_mask=APPLY_ELLIPSE,
-                                          mask_sigma=MASK_SIGMA, ellipse_dilation=ELLIPSE_DILATION, align=ALIGN)
-
-                # cv2.imshow('A1', cropA1)
-                # cv2.imshow('A2', cropA2)
-                # cv2.imshow('B', cropB)
-                # cv2.waitKey(0)
+                                         mask_sigma=MASK_SIGMA, ellipse_dilation=ELLIPSE_DILATION, align=ALIGN)
 
                 imgs += [[cropA1, cropA2]]
                 imgs += [[cropA1, cropB]]
@@ -113,39 +86,41 @@ if __name__ == '__main__':
                 i += 1
                 pbar.update(1)
             else:
-                print("random frame failed")
+                print("no tracklets in frame {}".format(frame))
 
     labels = np.array(labels)
     imgs = np.array(imgs)
 
-    #
     split_idx = int(TEST_RATIO * len(imgs))
-    #
     imgs_test = np.array(imgs[:split_idx])
     imgs_train = np.array(imgs[split_idx:])
     labels_test = np.array(labels[:split_idx])
     labels_train = np.array(labels[split_idx:])
-    #
-    print "imgs TEST: {}, TRAIN: {}".format(imgs_test.shape, imgs_train.shape)
-    print "labels TEST: {}, TRAIN: {}".format(labels_test.shape, labels_train.shape)
 
-    outdir = p.working_directory+''
-    # try:
-    #     outdir = args.outdir
-    # except:
-    #     pass
-    #
-    import h5py
+    print("imgs TEST: {}, TRAIN: {}".format(imgs_test.shape, imgs_train.shape))
+    print("labels TEST: {}, TRAIN: {}".format(labels_test.shape, labels_train.shape))
 
-    aligned = ''
     if ALIGN:
         aligned = '_aligned'
-
-    with h5py.File(outdir + '/descriptor_cnn_imgs_train'+aligned+'.h5', 'w') as hf:
+    else:
+        aligned = ''
+    with h5py.File(join(out_dir, 'descriptor_cnn_imgs_train{}.h5'.format(aligned)), 'w') as hf:
         hf.create_dataset("data", data=imgs_train)
-    with h5py.File(outdir + '/descriptor_cnn_imgs_test'+aligned+'.h5', 'w') as hf:
+    with h5py.File(join(out_dir, 'descriptor_cnn_imgs_test{}.h5'.format(aligned)), 'w') as hf:
         hf.create_dataset("data", data=imgs_test)
-    with h5py.File(outdir + '/descriptor_cnn_labels_train'+aligned+'.h5', 'w') as hf:
+    with h5py.File(join(out_dir, 'descriptor_cnn_labels_train{}.h5'.format(aligned)), 'w') as hf:
         hf.create_dataset("data", data=labels_train)
-    with h5py.File(outdir + '/descriptor_cnn_labels_test'+aligned+'.h5', 'w') as hf:
+    with h5py.File(join(out_dir, 'descriptor_cnn_labels_test{}.h5'.format(aligned)), 'w') as hf:
         hf.create_dataset("data", data=labels_test)
+
+
+if __name__ == '__main__':
+    # P_WD = '/Users/flipajs/Documents/wd/FERDA/april-paper/Cam1_clip'
+    # P_WD = '/Users/flipajs/Documents/wd/FERDA/april-paper/Sowbug3-crop'
+    # path = '/Users/flipajs/Documents/wd/FERDA/april-paper/5Zebrafish_nocover_22min'
+    # path = '/Users/flipajs/Documents/wd/FERDA/april-paper/Camera3-5min'
+    path = '../projects/Sowbug_deleteme2'
+    # P_WD = '/Users/flipajs/Documents/wd/FERDA/zebrafish_new'
+    generate_reidentification_training_data(path, path)
+
+

scripts/CNN/siamese_descriptor.py
@@ -1,85 +1,39 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import numpy as np
-
 import pickle
-import random
-from keras.datasets import mnist
+from os.path import join
 from keras.models import Model, load_model
 from keras.layers import Input, Flatten, Dense, Dropout, Lambda
-from keras.optimizers import RMSprop
-from keras import backend as K
 from keras.layers import Conv2D, MaxPooling2D, Input, Dense, Flatten, BatchNormalization, Activation
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint
 from tqdm import tqdm
 import argparse
-from scripts.CNN.train_siamese_contrastive_lost import contrastive_loss2
+
 from scripts.CNN.prepare_siamese_data import get_region_crop
 from scripts.CNN.prepare_siamese_data import ELLIPSE_DILATION, APPLY_ELLIPSE, OFFSET, MASK_SIGMA
-import cv2
-from keras.utils import CustomObjectScope
+from scripts.CNN.train_siamese_contrastive_lost import create_base_network10, euclidean_distance, eucl_dist_output_shape
+from core.project.project import Project
+
 
 def normalize_and_prepare_imgs(imgs):
     imgs = np.array(imgs)
     imgs = imgs.astype('float32')
     imgs /= 255
-
     return imgs
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='train siamese CNN with contrastive loss')
+def compute_descriptors(project_dir, model_weights_path, add_missing=False):
+    model = create_model(model_weights_path)
 
-    parser.add_argument('--datadir', type=str,
-                        # default='/Users/flipajs/Documents/wd/FERDA/april-paper/Cam1_clip_arena_fixed',
-                        default='/Users/flipajs/Documents/wd/FERDA/april-paper/Sowbug3-fixed-segmentation',
-                        # default='/Users/flipajs/Documents/wd/FERDA/april-paper/Camera3-5min',
-                        # default='/Users/flipajs/Documents/wd/FERDA/april-paper/Camera3-5min',
-                        # default='/Users/flipajs/Documents/wd/FERDA/april-paper/5Zebrafish_nocover_22min',
-                        help='path to dataset')
-
-    parser.add_argument('--add_missing', default=False, action='store_true',
-                        help='if used - only ids missing in descriptors.pkl will be computed')
-
-    args = parser.parse_args()
-    from scripts.CNN.train_siamese_contrastive_lost import create_base_network10, euclidean_distance, eucl_dist_output_shape
-
-    input_shape = (90, 90, 3)
-    input_a = Input(shape=input_shape)
-    input_b = Input(shape=input_shape)
-
-    architecture = create_base_network10
-    base_network = architecture(input_shape)
-    processed_a = base_network(input_a)
-    processed_b = base_network(input_b)
-
-    distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
-
-    model = Model([input_a, input_b], distance)
-    model.load_weights(args.datadir+"/best_model_998_weights.h5")
-    # model.load_weights(args.datadir+"/best_model_996_weights.h5")
-    # model.load_weights(args.datadir+"/best_model_980_weights.h5")
-    # model.load_weights(args.datadir+"/best_model_967_weights.h5")
-    # Cam1
-    # model.load_weights(args.datadir+"/best_model_996_weights.h5")
-    new_model = model.layers[2]
-
-    from core.project.project import Project
-    p = Project()
-    p.load(args.datadir)
+    p = Project(project_dir)
     vm = p.get_video_manager()
-
     descriptors = {}
-    if args.add_missing:
+    if add_missing:
         try:
-            with open(args.datadir + '/descriptors.pkl', 'rb') as f:
+            with open(join(project_dir, 'descriptors.pkl'), 'rb') as f:
                 descriptors = pickle.load(f)
         except:
             pass
-
-
     imgs = []
     r_ids = []
     batch_size = 300
@@ -92,13 +46,13 @@ if __name__ == '__main__':
             if region.id() in descriptors:
                 continue
 
-            crop = get_region_crop(region, img, APPLY_ELLIPSE, ELLIPSE_DILATION, MASK_SIGMA , OFFSET)
+            crop = get_region_crop(region, img, APPLY_ELLIPSE, ELLIPSE_DILATION, MASK_SIGMA, OFFSET)
             imgs.append(crop)
             r_ids.append(region.id())
 
         if len(imgs) >= batch_size:
             imgs = normalize_and_prepare_imgs(imgs)
-            descs = new_model.predict(imgs)
+            descs = model.predict(imgs)
 
             for k, r_id in enumerate(r_ids):
                 descriptors[r_id] = descs[k, :]
@@ -108,12 +62,35 @@ if __name__ == '__main__':
 
     # Do the rest
     imgs = normalize_and_prepare_imgs(imgs)
-    descs = new_model.predict(imgs)
-
+    descs = model.predict(imgs)
     for k, r_id in enumerate(r_ids):
         descriptors[r_id] = descs[k, :]
-
-    with open(args.datadir+'/descriptors.pkl', 'wb') as f:
+    with open(join(project_dir, 'descriptors.pkl'), 'wb') as f:
         pickle.dump(descriptors, f)
-
     print("DONE")
+
+
+def create_model(model_weights_path):
+    input_shape = (90, 90, 3)
+    input_a = Input(shape=input_shape)
+    input_b = Input(shape=input_shape)
+    architecture = create_base_network10
+    base_network = architecture(input_shape)
+    processed_a = base_network(input_a)
+    processed_b = base_network(input_b)
+    distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
+    model_ = Model([input_a, input_b], distance)
+    model_.load_weights(model_weights_path)
+    model = model_.layers[2]
+    return model
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='compute reidentification descriptors for tracklets')
+    parser.add_argument('--weights', type=str, help='filename of the CNN model weights')
+    parser.add_argument('--project-dir', type=str, help='project directory')
+    parser.add_argument('--add-missing', default=False, action='store_true',
+                        help='if used - only ids missing in descriptors.pkl will be computed')
+
+    args = parser.parse_args()
+    compute_descriptors(args.project_dir, args.weights, args.add_missing)