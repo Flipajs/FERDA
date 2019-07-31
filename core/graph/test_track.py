3d1c908b4feb31f47ea0a543d820bd60a3232cef
Matěj Šmíd <m@matejsmid.cz>
2019-07-31 18:39:13 +0200

    Track: add overlap test, merging two tracks, adding tracklets to track, add visualization, add unit tests

---
+++ core/graph/test_track.py
@@ -0,0 +1,73 @@
+import unittest
+from track import Track
+from core.graph.graph_manager import GraphManager
+from core.project.project import Project
+from intervals.interval import IntInterval
+from core.region.region import Region
+
+
+class TestTrack(unittest.TestCase):
+    def setUp(self):
+        self.p = Project('test/project/Sowbug3_cut_300_frames')
+        """
+        [(t.id_, t.get_interval()) for t in self.p.chm.tracklet_gen()]
+        [(1, IntInterval('[0, 300]')),
+         (2, IntInterval('[0, 300]')),
+         (749, IntInterval('[243, 252]')),
+         (4, IntInterval('[0, 141]')),
+         (5, IntInterval('[0, 193]')),
+         (737, IntInterval('[241, 242]')),
+         (827, IntInterval('[259, 300]')),
+         (747, IntInterval('[243, 258]')),
+         (738, IntInterval('[241, 252]')),
+         (694, IntInterval('[232, 242]')),
+         (693, IntInterval('[232, 240]')),
+         (502, IntInterval('[194, 231]')),
+         (503, IntInterval('[194, 231]')),
+         (346, IntInterval('[142, 240]')),
+         (347, IntInterval('[142, 193]')),
+         (828, IntInterval('[259, 300]')),
+         (798, IntInterval('[253, 300]')),
+         (799, IntInterval('[253, 258]'))]
+        """
+        self.track = Track([self.p.chm[i] for i in [4, 346]], self.p.gm)
+
+    def test_get_interval(self):
+        interval = self.track.get_interval()
+        self.assertEqual(len(interval), 2)
+        self.assertFalse(reduce(IntInterval.is_connected, self.track.get_interval()))
+
+    def test_is_overlapping(self):
+        t1 = Track([self.p.chm[i] for i in [5]], self.p.gm)
+        self.assertEqual(t1.get_interval()[0].lower, 0)
+        self.assertEqual(t1.get_interval()[0].upper, 193)
+        t2 = Track([self.p.chm[i] for i in [749, 798]], self.p.gm)
+        self.assertEqual(t2.get_interval()[0].lower, 243)
+        self.assertEqual(t2.get_interval()[0].upper, 252)
+        self.assertEqual(t2.get_interval()[1].lower, 253)
+        self.assertEqual(t2.get_interval()[1].upper, 300)
+        self.assertFalse(t1.is_overlapping(t2))
+        self.assertFalse(t2.is_overlapping(t1))
+
+        t1 = Track([self.p.chm[i] for i in [749]], self.p.gm)
+        self.assertEqual(t1.get_interval()[0].lower, 243)
+        self.assertEqual(t1.get_interval()[0].upper, 252)
+        t2 = Track([self.p.chm[i] for i in [798]], self.p.gm)
+        self.assertEqual(t2.get_interval()[0].lower, 253)
+        self.assertEqual(t2.get_interval()[0].upper, 300)
+        self.assertFalse(t1.is_overlapping(t2))
+        self.assertFalse(t2.is_overlapping(t1))
+
+        t1 = Track([self.p.chm[i] for i in [502]], self.p.gm)
+        self.assertEqual(t1.get_interval()[0].lower, 194)
+        self.assertEqual(t1.get_interval()[0].upper, 231)
+        t2 = Track([self.p.chm[i] for i in [346]], self.p.gm)
+        self.assertEqual(t2.get_interval()[0].lower, 142)
+        self.assertEqual(t2.get_interval()[0].upper, 240)
+        self.assertTrue(t1.is_overlapping(t2))
+        self.assertTrue(t2.is_overlapping(t1))
+
+
+
+
+
--- core/graph/track.py
+++ core/graph/track.py
@@ -3,6 +3,8 @@
 from chunk import Chunk
 from ghost_tracklet import GhostTracklet
 from warnings import warn
+from intervals import IntInterval
+import itertools
 
 
 def unfold(tracklets):
@@ -18,41 +20,17 @@ def unfold(tracklets):
 
 
 class Track(Chunk):
-    def __init__(self, tracklets, gm, id_=-1):
-        self.id_ = id_
+    def __init__(self, tracklets, gm, id_=-1, ghost_tracklets=False):
+        super(Track, self).__init__(None, id_, gm)
 
         # tracklets and spaces...
-        self._data=[]
-        tracklets = sorted(tracklets, key=lambda x: x.start_frame())
-
-        self.P = set()
-        self.N = set()
-
+        self._data = []
         self._num_regions = 0
 
-        for t in tracklets:
-            if t.is_track():
-                new_track = Track(unfold(tracklets), gm, id_)
-                self.__dict__ = new_track.__dict__
-                return
-
-        for t in tracklets:
-            sf = t.start_frame()
-            if len(self._data):
-                ef = self._data[-1].end_frame()
-                if ef + 1 != sf:
-                    self._data.append(GhostTracklet(ef, sf - 1))
-
-            self.P = self.P.union(t.P)
-            self.N = self.N.union(t.N)
-
-            self._data.append(t)
-
-            self._num_regions += len(t)
+        self.extend(tracklets, ghost_tracklets)
 
         self.color = self._data[0].color
         self.segmentation_class = self._data[0].segmentation_class
-
         self.start = self._data[0].start_frame()
         self.end = self._data[-1].end_frame()
 
@@ -102,25 +80,67 @@ class Track(Chunk):
     def num_regions(self):
         return self._num_regions
 
-    # gm is tracklet compability
     def start_frame(self):
         return self.start
 
-    # gm is tracklet compability
     def end_frame(self):
         return self.end
 
-    def add_tracklet(self, tracklet, gm):
-        # TODO: possible speedup
-        my_tracklets = [tracklet]
-        for t in self._data:
-            if isinstance(Chunk):
-                my_tracklets.append(t)
+    def tracklets(self):
+        return self._data
 
-        self.__dict__.update(Track(my_tracklets, gm, id_=self.id_).__dict__)
+    def _sort_tracklets(self):
+        self._data = sorted(self._data, key=lambda x: x.start_frame())
+        self.start = self._data[0].start_frame()
+        self.end = self._data[-1].end_frame()
+
+    def append(self, tracklet, sort=True, ghost_tracklets=False):
+        if ghost_tracklets:
+            sf = tracklet.start_frame()
+            if len(self._data):
+                ef = self._data[-1].end_frame()
+                if ef + 1 != sf:
+                    self._data.append(GhostTracklet(ef, sf - 1))
+        self._data.append(tracklet)
+        self.P = self.P.union(tracklet.P)
+        self.N = self.N.union(tracklet.N)
+        self._num_regions += len(tracklet)
+        assert self.is_consistent()
+        if sort:
+            self._sort_tracklets()
+
+    def extend(self, tracklets, ghost_tracklets=False):
+        for t in tracklets:
+            self.append(t, sort=False, ghost_tracklets=ghost_tracklets)
+        self._sort_tracklets()
+
+    def merge(self, other_track):
+        P = self.P.copy()
+        N = self.N.copy()
+        self.extend(other_track.tracklets())
+        self.P = P
+        self.N = N
 
     def __len__(self):
         return self.end - self.start + 1
+
+    def get_temporal_intervals(self):
+        return [(t.start_frame(), t.end_frame()) for t in self._data]
+
+    def get_interval(self):
+        return [t.get_interval() for t in self._data]
+
+    def is_overlapping(self, other):
+        for t1, t2 in itertools.product(self.tracklets(), other.tracklets()):
+            if t1.is_overlapping(t2):
+                return True
+        return False
+
+    def is_consistent(self):
+        for t1, t2 in itertools.combinations(self.tracklets(), 2):
+            if t1.is_overlapping(t2):
+                return False
+        return True
 
     def print_info(self):
         s = "TRACKLET --- id: "+str(self.id_)+" length: "+str(len(self.nodes_))+"\n"
@@ -140,9 +160,6 @@ class Track(Chunk):
     def pop_last(self, undo_action=False):
         warn("Not implemented for Track...")
 
-    def merge(self, ch2):
-        warn("Not implemented for Track...")
-
     def merge_and_interpolate(self, ch2):
         warn("Not implemented for Track...")
 
@@ -159,13 +176,13 @@ class Track(Chunk):
         return self._data[-1].nodes_[-1]
 
     def end_vertex(self):
-        return gm.g.vertex(self.end_vertex_id())
+        return self.gm.g.vertex(self.end_vertex_id())
 
     def end_node(self):
         return self.end_vertex_id()
 
     def start_vertex(self):
-        return gm.g.vertex(self.start_vertex_id())
+        return self.gm.g.vertex(self.start_vertex_id())
 
     def start_node(self):
         return self._data[0].start_vertex_id()
@@ -181,10 +198,10 @@ class Track(Chunk):
     def rid_gen(self):
         for t in self._data:
             for id_ in t.nodes_:
-                yield gm.region_id(id_)
+                yield self.gm.region_id(id_)
 
-    def v_id_in_t(self, t):
-        tracklet = self._tracklet(frame, gm)
+    def v_id_in_t(self, frame):
+        tracklet = self._tracklet(frame)
 
         frame = frame - self.start_frame()
         if -1 < frame < len(tracklet.nodes_):
@@ -192,16 +209,16 @@ class Track(Chunk):
         else:
             return None
 
-    def _tracklet(self, frame, gm):
+    def _tracklet(self, frame):
         # TODO: improve...
         for t in self._data:
-            if frame >= t.start_frame() and frame <= t.end_frame():
+            if t.start_frame() <= frame <= t.end_frame():
                 return t
 
-    def r_id_in_t(self, t):
-        tracklet = self._tracklet(frame, gm)
+    def r_id_in_t(self, frame):
+        tracklet = self._tracklet(frame)
         # TODO: find proper chunk...
-        return gm.region_id(tracklet.v_id_in_t(frame))
+        return self.gm.region_id(tracklet.v_id_in_t(frame))
 
     def is_tracklet(self):
         return False
@@ -215,3 +232,11 @@ class Track(Chunk):
                 return True
 
         return False
+
+    def draw(self, rm, *args, **kwargs):
+        import matplotlib.pylab as plt
+        for t in self._data:
+            t.draw(rm, *args, **kwargs)
+        xy = self._data[0].get_region(0).centroid()[::-1]
+        plt.annotate('track {}'.format(self.id()), xy=xy, textcoords='offset pixels', xytext=(-10, -10), color='w')
+
